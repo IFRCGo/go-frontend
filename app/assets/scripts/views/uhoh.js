2bf0fd480b53670d24c8a06b342b92660fe80163
Daniel da Silva <daniel@developmentseed.org>
2017-11-20 17:46:46 +0000

    Update uhOh page

--- app/assets/scripts/views/uhoh.js
+++ app/assets/scripts/views/uhoh.js
@@ -1,25 +1,29 @@
 'use strict';
 import React from 'react';
 
+import App from './app';
+
 export default class UhOh extends React.Component {
   render () {
     return (
-      <section className='inpage inpage--uhoh'>
-        <header className='inpage__header'>
-          <div className='inner'>
-            <div className='inpage__headline'>
-              <h1 className='inpage__title'>Page not found</h1>
+      <App className='page--uhoh'>
+        <section className='inpage inpage--uhoh'>
+          <header className='inpage__header'>
+            <div className='inner'>
+              <div className='inpage__headline'>
+                <h1 className='inpage__title'>Page not found</h1>
+              </div>
+            </div>
+          </header>
+          <div className='inpage__body'>
+            <div className='inner'>
+              <div className='prose prose--responsive'>
+                <p>The requested page does not exist or may have been removed.</p>
+              </div>
             </div>
           </div>
-        </header>
-        <div className='inpage__body'>
-          <div className='inner'>
-            <div className='prose prose--responsive'>
-              <p>The requested page does not exist or may have been removed.</p>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </App>
     );
   }
 }
