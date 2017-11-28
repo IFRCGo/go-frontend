b415474325473e97564e6ccfee8f75503ba88614
Daniel da Silva <daniel@developmentseed.org>
2017-11-28 12:28:45 +0000

    Add system alerts

--- app/assets/scripts/reducers/index.js
+++ app/assets/scripts/reducers/index.js
@@ -2,9 +2,11 @@
 import { combineReducers } from 'redux';
 
 import user from './user';
+import { systemAlertsReducer } from '../components/system-alerts';
 
 export const reducers = {
-  user
+  user,
+  systemAlertsReducer
 };
 
 export default combineReducers(reducers);
