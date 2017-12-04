78ba50d218b074ec2f7c105d2b7d89a97b833e84
Daniel da Silva <daniel@developmentseed.org>
2017-12-04 13:25:22 +0000

    Add homepage title

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -11,14 +11,14 @@ export default class Home extends React.Component {
           <header className='inpage__header'>
             <div className='inner'>
               <div className='inpage__headline'>
-                <h1 className='inpage__title'>Welcome</h1>
+                <h1 className='inpage__title'>IFRC Disaster Response and Prepardness</h1>
+                <p className='inpage__introduction'>In the beginning the Universe was created. This has made a lot of people very upset and been widely regarded as a bad move.</p>
               </div>
             </div>
           </header>
           <div className='inpage__body'>
             <div className='inner'>
               <div className='prose prose--responsive'>
-                <p>In the beginning the Universe was created. This has made a lot of people very upset and been widely regarded as a bad move.</p>
               </div>
             </div>
           </div>
