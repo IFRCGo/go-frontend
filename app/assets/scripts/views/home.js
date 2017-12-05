2da29dad6fe4c83d973819bd62bdf7537642d768
Daniel da Silva <daniel@developmentseed.org>
2017-12-05 17:22:45 +0000

    Use self hosted tiles

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -156,9 +156,60 @@ class Map extends React.Component {
   setupMap () {
     mapboxgl.accessToken = mbtoken;
 
+    const mapStyle = {
+      'version': 8,
+      'sources': {
+        'ifrc': {
+          'type': 'vector',
+          'tiles': ['https://dsgofilestorage.blob.core.windows.net/tiles/{z}/{x}/{y}.pbf']
+        }
+      },
+      'layers': [
+        {
+          'id': 'background',
+          'type': 'background',
+          'paint': {
+            'background-color': 'hsl(218, 38%, 22%)'
+          }
+        },
+        {
+          'id': 'country',
+          'type': 'fill',
+          'source': 'ifrc',
+          'source-layer': 'country',
+          'filter': [
+            '!in',
+            'ADMIN',
+            'Antarctica'
+          ],
+          'layout': {
+            'visibility': 'visible'
+          },
+          'paint': {
+            'fill-color': 'hsl(213, 38%, 28%)',
+            'fill-opacity': 1,
+            'fill-outline-color': 'hsla(209, 16%, 50%, 0.68)'
+          }
+        },
+        {
+          'id': 'population',
+          'type': 'circle',
+          'source': 'ifrc',
+          'source-layer': 'population',
+          'layout': {
+            'visibility': 'visible'
+          },
+          'paint': {
+            'circle-radius': 2,
+            'circle-color': 'hsl(210, 77%, 11%)'
+          }
+        }
+      ]
+    };
+
     this.theMap = new mapboxgl.Map({
       container: this.refs.map,
-      style: 'mapbox://styles/devseed/cjak4za9kbgsv2rn0ivoiuobd',
+      style: mapStyle,
       attributionControl: false
     });
   }
