e750d81924bbd765160296bb06887f9db1b84f60
Derek Lieu <dl@dereklieu.com>
2017-12-13 18:53:51 -0500

    Hook up recent emergencies table

---
+++ app/assets/scripts/components/connected/emergencies-table.js
@@ -0,0 +1,175 @@
+'use strict';
+import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes as T } from 'prop-types';
+import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import { DateTime } from 'luxon';
+
+import { environment } from '../../config';
+import { getEmergenciesList } from '../../actions';
+import { nope, commaSeparatedNumber as n } from '../../utils/format';
+import { get } from '../../utils/utils';
+
+import Fold from '../fold';
+import { showGlobalLoading, hideGlobalLoading } from '../global-loading';
+
+class EmergenciesTable extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      page: 1
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  componentDidMount () {
+    this.requestResults();
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.list.fetching && !nextProps.list.fetching) {
+      hideGlobalLoading();
+    }
+  }
+
+  requestResults () {
+    showGlobalLoading();
+    this.props._getEmergenciesList(this.state.page);
+  }
+
+  handlePageChange (page) {
+    this.setState({ page: page.selected + 1 }, () => {
+      this.requestResults();
+    });
+  }
+
+  renderRow (rowData, idx, all) {
+    const isLast = idx === all.length - 1;
+
+    const disasterDate = rowData.disaster_start_date ||
+      rowData.start_date ||
+      rowData.created_at;
+
+    const date = disasterDate
+      ? DateTime.fromISO(disasterDate).toISODate() : nope;
+
+    const beneficiaries = get(rowData, 'appeals', []).reduce((acc, next) => {
+      return acc + next.num_beneficiaries;
+    }, 0);
+
+    const countries = get(rowData, 'countries', []).map(c => (
+      <Link key={`c.iso`} to={`/country/${c.id}`}>{c.name}</Link>
+    ));
+
+    return (
+      <React.Fragment key={rowData.id}>
+        <tr>
+          <td data-heading='Date'>{date}</td>
+          <td data-heading='Name'>{get(rowData, 'name', nope)}</td>
+          <td data-heading='Disaster Type'>{get(rowData, 'dtype.name', nope)}</td>
+          <td data-heading='Total Affected'>{n(get(rowData, 'num_affected'))}</td>
+          <td data-heading='Beneficiaries'>{n(beneficiaries)}</td>
+          <td data-heading='Countries'>{countries.length ? countries : nope}</td>
+        </tr>
+
+        {!isLast && (
+          <tr role='presentation'>
+            <td colSpan='4'></td>
+          </tr>
+        )}
+      </React.Fragment>
+    );
+  }
+
+  render () {
+    const {
+      data,
+      fetched,
+      receivedAt,
+      error
+    } = this.props.list;
+    console.log(data);
+
+    if (!receivedAt && !fetched) return null;
+
+    if (error) {
+      return (
+        <Fold title='Latest Alerts'>
+          <p>Oh no! An error ocurred getting the stats.</p>
+        </Fold>
+      );
+    }
+
+    if (!data.objects.length) {
+      return (
+        <Fold title='Latest Alerts'>
+          <p>There are no results to show.</p>
+        </Fold>
+      );
+    }
+
+    return (
+      <Fold title={`Latest Emergencies (${n(data.meta.total_count)})`}>
+        <div className='table-scroll'>
+          <div className='table-wrap'>
+            <table className='table table--zebra responsive-table'>
+              <thead>
+                <tr>
+                  <th className='fixed-col'>Date</th>
+                  <th>Name</th>
+                  <th>Disaster Type</th>
+                  <th>Total Affected</th>
+                  <th>Benficiaries</th>
+                  <th>Countries</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.objects.map(this.renderRow)}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        {data.objects.length !== 0 && (
+          <div className='pagination-wrapper'>
+            <ReactPaginate
+              previousLabel={<span>previous</span>}
+              nextLabel={<span>next</span>}
+              breakLabel={<span className='pages__page'>...</span>}
+              pageCount={Math.ceil(data.meta.total_count / data.meta.limit)}
+              forcePage={data.meta.offset / data.meta.limit}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.handlePageChange}
+              containerClassName={'pagination'}
+              subContainerClassName={'pages'}
+              pageClassName={'pages__wrapper'}
+              pageLinkClassName={'pages__page'}
+              activeClassName={'active'} />
+          </div>
+        )}
+      </Fold>
+    );
+  }
+}
+
+if (environment !== 'production') {
+  EmergenciesTable.propTypes = {
+    _getEmergenciesList: T.func,
+    list: T.object
+  };
+}
+
+// /////////////////////////////////////////////////////////////////// //
+// Connect functions
+
+const selector = (state) => ({
+  list: state.emergencies.list
+});
+
+const dispatcher = (dispatch) => ({
+  _getEmergenciesList: (...args) => dispatch(getEmergenciesList(...args))
+});
+
+export default connect(selector, dispatcher)(EmergenciesTable);
--- app/assets/scripts/views/emergencies.js
+++ app/assets/scripts/views/emergencies.js
@@ -6,6 +6,8 @@ import { PropTypes as T } from 'prop-types';
 
 import App from './app';
 import EmergenciesDash from '../components/connected/emergencies-dash';
+import EmergenciesTable from '../components/connected/emergencies-table';
+
 import { getLastMonthsEmergencies, getAggregateEmergencies } from '../actions';
 import { finishedFetch } from '../utils/utils';
 import { showGlobalLoading, hideGlobalLoading } from '../components/global-loading';
@@ -33,64 +35,11 @@ class Emergencies extends React.Component {
       <App className='page--emergencies'>
         <section className='inpage'>
           <EmergenciesDash />
-
           <div className='inpage__body'>
             <div className='map'>
             </div>
             <div className='inner'>
-
-              <section className='fold'>
-                <div className='inner'>
-                  <div className='fold__header'>
-                    <h2 className='fold__title'>Latest Emergencies (2301)</h2>
-                  </div>
-                  <div className='fold__body'>
-                    <div className='table-scroll'>
-                      <div className='table-wrap'>
-                        <table className='table table--zebra'>
-                          <thead>
-                            <tr>
-                              <th className='fixed-col'>Date</th>
-                              <th>Name</th>
-                              <th>Disaster Type</th>
-                              <th>Total Affected</th>
-                              <th>Benficiaries</th>
-                              <th>Countries</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td className='fixed-col'>07/10/2017</td>
-                              <td><a href=''className='link--primary'>Typhoon Damrery 2017</a></td>
-                              <td>Topical Cyclone</td>
-                              <td>743,466</td>
-                              <td>743,466</td>
-                              <td><a href=''className='link--primary'>Country 1</a><a href=''className='link--primary'>Country 2</a><a href=''className='link--primary'>Country 3</a></td>
-                            </tr>
-                            <tr>
-                              <td className='fixed-col'>07/10/2017</td>
-                              <td><a href=''className='link--primary'>Typhoon Damrery 2017</a></td>
-                              <td>Topical Cyclone</td>
-                              <td>743,466</td>
-                              <td>743,466</td>
-                              <td><a href=''className='link--primary'>Country 1</a><a href=''className='link--primary'>Country 2</a><a href=''className='link--primary'>Country 3</a></td>
-                            </tr>
-                            <tr>
-                              <td className='fixed-col'>07/10/2017</td>
-                              <td><a href=''className='link--primary'>Typhoon Damrery 2017</a></td>
-                              <td>Topical Cyclone</td>
-                              <td>743,466</td>
-                              <td>743,466</td>
-                              <td><a href=''className='link--primary'>Country 1</a><a href=''className='link--primary'>Country 2</a><a href=''className='link--primary'>Country 3</a></td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-
+              <EmergenciesTable />
             </div>
           </div>
         </section>
