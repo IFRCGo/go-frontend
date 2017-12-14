5dd0bc0e73fed7154cb1c8ac1c3a3bf29af5564b
Derek Lieu <dl@dereklieu.com>
2017-12-14 10:38:09 -0500

    Create a util to centralize map config

--- app/assets/scripts/components/homemap.js
+++ app/assets/scripts/components/homemap.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { PropTypes as T } from 'prop-types';
-import mapboxgl from 'mapbox-gl';
 import c from 'classnames';
+import mapboxgl from 'mapbox-gl';
 
-import { environment, mbtoken } from '../config';
+import { environment } from '../config';
 import {
   FormRadioGroup
 } from './form-elements/';
+import newMap from '../utils/get-new-map';
 import Progress from './progress';
 import BlockLoading from './block-loading';
 import MapErrorBoundary from './map-error-boundary';
@@ -184,94 +185,14 @@ class Map extends React.Component {
     this.mapLoaded = false;
     this.popover = null;
 
-    mapboxgl.accessToken = mbtoken;
-
-    const mapStyle = {
-      'version': 8,
-      'sources': {
-        'ifrc': {
-          'type': 'vector',
-          'tiles': ['https://dsgofilestorage.blob.core.windows.net/tiles/{z}/{x}/{y}.pbf']
-        }
-      },
-      'layers': [
-        {
-          'id': 'background',
-          'type': 'background',
-          'paint': {
-            'background-color': 'hsl(218, 38%, 22%)'
-          }
-        },
-        {
-          'id': 'country',
-          'type': 'fill',
-          'source': 'ifrc',
-          'source-layer': 'country',
-          'filter': [
-            '!in',
-            'ADMIN',
-            'Antarctica'
-          ],
-          'layout': {
-            'visibility': 'visible'
-          },
-          'paint': {
-            'fill-color': 'hsl(213, 38%, 28%)',
-            'fill-opacity': 1,
-            'fill-outline-color': 'hsla(209, 16%, 50%, 0.68)'
-          }
-        },
-        {
-          'id': 'population',
-          'type': 'circle',
-          'source': 'ifrc',
-          'source-layer': 'population',
-          'layout': {
-            'visibility': 'visible'
-          },
-          'paint': {
-            'circle-radius': 2,
-            'circle-color': 'hsl(210, 77%, 11%)'
-          }
-        }
-      ]
-    };
-
-    this.theMap = new mapboxgl.Map({
-      container: this.refs.map,
-      style: mapStyle,
-      zoom: 1,
-      maxZoom: 3.5,
-      scrollZoom: false,
-      center: [6, 15],
-      pitchWithRotate: false,
-      dragRotate: false,
-      renderWorldCopies: false,
-      maxBounds: [
-        [-220, -70],
-        [220, 70]
-      ],
-      attributionControl: false
-    });
+    this.theMap = newMap(this.refs.map);
 
     this.theMap.on('style.load', () => {
       this.mapLoaded = true;
       this.setupData();
     });
 
-    this.theMap.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-    // Disable map rotation using right click + drag.
-    this.theMap.dragRotate.disable();
-
-    // Disable map rotation using touch rotation gesture.
-    this.theMap.touchZoomRotate.disableRotation();
-
-    // Remove compass.
-    document.querySelector('.mapboxgl-ctrl .mapboxgl-ctrl-compass').remove();
-
     // Event listeners.
-
     this.theMap.on('click', 'appeals', e => {
       this.showPopover(e.features[0]);
     });
