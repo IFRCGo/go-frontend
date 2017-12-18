0f823fc966db7192359bc56a70ff530dc4edff94
Derek Lieu <dl@dereklieu.com>
2017-12-18 14:24:05 -0500

    Include bar charts and surge alert table

--- app/assets/scripts/views/deployments.js
+++ app/assets/scripts/views/deployments.js
@@ -1,13 +1,19 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
-
-import App from './app';
+import { PropTypes as T } from 'prop-types';
 
 import { getEruOwners } from '../actions';
-import { finishedFetch, get } from '../utils/utils';
+import { finishedFetch } from '../utils/utils';
 import { showGlobalLoading, hideGlobalLoading } from '../components/global-loading';
 import { environment } from '../config';
+import {
+  commaSeparatedNumber as n
+} from '../utils/format';
+
+import App from './app';
+import AlertsTable from '../components/connected/alerts-table';
+import Progress from '../components/progress';
 
 class Deployments extends React.Component {
   componentDidMount () {
@@ -21,18 +27,96 @@ class Deployments extends React.Component {
     }
   }
 
+  renderHeaderCharts (data, title) {
+    const max = Math.max.apply(Math, data.map(o => +o.items));
+    const items = data.length > 6 ? data.slice(0, 6) : data;
+    return (
+      <div>
+        <h2>{title}</h2>
+        <div className='emergencies__container'>
+          <ul className='emergencies__list'>
+            {items.map(o => (
+              <li key={o.name}
+                className='emergencies__item'>
+                <span className='key'>{o.name}</span>
+                <span className='value'><Progress value={o.items} max={max}><span>100</span></Progress></span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  renderHeaderStats () {
+    const { data } = this.props.eruOwners;
+    return (
+      <div className='inpage__introduction'>
+        <div className='header-stats'>
+          <ul className='stats-list'>
+            <li className='stats-list__item stats-emergencies'>
+              {n(data.deployed)}<small>Deployed ERUs</small>
+            </li>
+            <li className='stats-list__item stats-funding stat-borderless stat-double'>
+              {n(data.ready)}<small>Ready ERUs</small>
+            </li>
+          </ul>
+        </div>
+        <div className='inpage__headline-charts'>
+          <div className='stats-chart'>
+            {this.renderHeaderCharts(data.types, 'ERU Deployment Types')}
+            {this.renderHeaderCharts(data.owners, 'Number of Deployments by NS')}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderContent () {
+    const {
+      fetched,
+      error,
+      data
+    } = this.props.eruOwners;
+
+    if (!fetched || error) return null;
+
+    return (
+      <section className='inpage'>
+        <header className='inpage__header'>
+          <div className='inner'>
+            <div className='inpage__headline'>
+              <div className='inpage__headline-content'>
+                <h1 className='inpage__title'>Deployments</h1>
+                {this.renderHeaderStats()}
+              </div>
+            </div>
+          </div>
+        </header>
+        <pre>
+          {JSON.stringify(data, null, 2)}
+        </pre>
+        <div>
+          <AlertsTable />
+        </div>
+      </section>
+    );
+  }
+
   render () {
-    const data = get(this.props.eruOwners, 'data');
     return (
       <App className='page--deployments'>
-        <section className='inpage'>
-          <pre>
-            {JSON.stringify(data, null, 2)}
-          </pre>
-        </section>
+        {this.renderContent()}
       </App>
     );
   }
+}
+
+if (environment !== 'production') {
+  Deployments.propTypes = {
+    _getEruOwners: T.func,
+    eruOwners: T.object
+  };
 }
 
 const selector = (state) => ({
