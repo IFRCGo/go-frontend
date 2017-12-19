0e15d651568850ff2306e9b69e5a4a7236306d38
Derek Lieu <dl@dereklieu.com>
2017-12-19 17:28:58 -0500

    Fix spelling in site header

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -13,7 +13,7 @@ export default class Home extends React.Component {
           <header className='inpage__header'>
             <div className='inner'>
               <div className='inpage__headline'>
-                <h1 className='inpage__title'>IFRC Disaster Response and Prepardness</h1>
+                <h1 className='inpage__title'>IFRC Disaster Response and Preparedness</h1>
                 <p className='inpage__introduction'>In the beginning the Universe was created. This has made a lot of people very upset and been widely regarded as a bad move.</p>
               </div>
             </div>
