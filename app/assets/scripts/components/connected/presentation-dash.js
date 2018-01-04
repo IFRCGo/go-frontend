9d893ea7d9eeeacb456b71b1be6db3507bba91e6
Anna Scalamogna <anna@developmentseed.org>
2018-01-04 14:12:28 -0500

    update text throughout

--- app/assets/scripts/components/connected/presentation-dash.js
+++ app/assets/scripts/components/connected/presentation-dash.js
@@ -96,7 +96,6 @@ class PresentationDash extends React.Component {
 
     return (
       <section className={c('fold--stats', {presenting: this.state.fullscreen})} id='presentation'>
-        <h1 className='visually-hidden'>Statistics</h1>
         <div className='inner'>
           <div className='presentation__actions'>
             <button className='button button--base-plain button--fullscreen' onClick={this.toggleFullscreen} title='View in fullscreen'><span>FullScreen</span></button>
