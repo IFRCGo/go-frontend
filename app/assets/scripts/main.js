0eeffd7216d7fd00402e78d289a20eb933998d5b
Daniel da Silva <daniel@developmentseed.org>
2018-01-05 19:23:22 +0000

    Add polyfill for HTML Node remove

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -107,3 +107,21 @@ render(
   <Root store={store} />,
   document.querySelector('#app-container')
 );
+
+// Polyfill for HTML Node remove();
+// https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove () {
+        if (this.parentNode !== null) this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
