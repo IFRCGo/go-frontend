ee2b2084552427e9a6579ef32aecd90204fc8a64
Daniel da Silva <daniel@developmentseed.org>
2018-01-10 12:38:50 +0000

    Remove reverse() from aggregate data
    Api now returns data in the correct order

--- app/assets/scripts/reducers/emergencies.js
+++ app/assets/scripts/reducers/emergencies.js
@@ -175,7 +175,7 @@ function aggregate (state = aggregateInitialState, action) {
           fetching: false,
           fetched: true,
           receivedAt: action.receivedAt,
-          data: action.data.aggregate.reverse()
+          data: action.data.aggregate
         }
       });
       break;
