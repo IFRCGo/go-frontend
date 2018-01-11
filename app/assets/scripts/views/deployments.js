c7b0ac83599b99b4032a3b7a05603deb5b8caa4a
Daniel da Silva <daniel@developmentseed.org>
2018-01-11 15:40:37 +0000

    Prepare table with component

--- app/assets/scripts/views/deployments.js
+++ app/assets/scripts/views/deployments.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes as T } from 'prop-types';
+import { DateTime } from 'luxon';
 
 import { getEruOwners } from '../actions';
 import { finishedFetch } from '../utils/utils';
@@ -10,14 +11,49 @@ import { environment } from '../config';
 import {
   commaSeparatedNumber as n
 } from '../utils/format';
+import { dateOptions, datesAgo } from '../utils/utils/';
 
 import App from './app';
-import AlertsTable from '../components/connected/alerts-table';
+import Fold from '../components/fold';
 import Progress from '../components/progress';
+import BlockLoading from '../components/block-loading';
+import { SFPComponent } from '../utils/extendables';
+import DisplayTable, { SortHeader, FilterHeader } from '../components/display-table';
 import Map from '../components/deployments/map';
 import Readiness from '../components/deployments/readiness';
 
-class Deployments extends React.Component {
+// TODO: Use correct ids.
+const deployTypes = [
+  { value: 'all', label: 'All' },
+  { value: 'X', label: 'ERU' },
+  { value: 'Xx', label: 'FACT' },
+  { value: 'Xxx', label: 'RT-RIT' },
+  { value: 'Xxxx', label: 'HeOps' }
+];
+
+class Deployments extends SFPComponent {
+  // Methods form SFPComponent:
+  // handlePageChange (what, page)
+  // handleFilterChange (what, field, value)
+  // handleSortChange (what, field)
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      deployments: {
+        page: 1,
+        sort: {
+          field: '',
+          direction: 'asc'
+        },
+        filters: {
+          date: 'all',
+          type: 'all'
+        }
+      }
+    };
+  }
+
   componentDidMount () {
     showGlobalLoading();
     this.props._getEruOwners();
@@ -27,6 +63,30 @@ class Deployments extends React.Component {
     if (finishedFetch(this.props, nextProps, 'eruOwners')) {
       hideGlobalLoading();
     }
+  }
+
+  requestResults () {
+    let qs = {};
+    let state = this.state.deployments;
+    if (state.sort.field) {
+      qs.order_by = (state.sort.direction === 'desc' ? '-' : '') + state.sort.field;
+    }
+
+    if (state.filters.date !== 'all') {
+      qs.created_at__gte = datesAgo[state.filters.date]();
+    }
+
+    // TODO: type filter
+    if (state.filters.type !== 'all') {
+      // qs.SOMETHING = state.filters.type;
+    }
+
+    // TODO: Implement function
+    // this.props._fn(this.state.deployments.page, qs);
+  }
+
+  updateData (what) {
+    this.requestResults();
   }
 
   renderHeaderCharts (data, title) {
@@ -82,6 +142,97 @@ class Deployments extends React.Component {
     );
   }
 
+  renerDeploymentsTable () {
+    // const {
+    //   fetched,
+    //   fetching,
+    //   error,
+    //   data
+    // // TODO: Get correct property
+    // } = this.props.SOMETHING;
+    const fetched = true;
+    const fetching = false;
+    const error = false;
+    const data = {
+      meta: {
+        total_count: 10,
+        limit: 5,
+        offset: 0
+      }
+    };
+
+    if (fetching) {
+      return (
+        <Fold title='Current Deployments'>
+          <BlockLoading/>
+        </Fold>
+      );
+    }
+
+    if (error) {
+      return (
+        <Fold title='Current Deployments'>
+          <p>Oh no! An error ocurred getting the data.</p>
+        </Fold>
+      );
+    }
+
+    if (fetched) {
+      const headings = [
+        {
+          id: 'date',
+          label: <FilterHeader id='date' title='Star Date' options={dateOptions} filter={this.state.deployments.filters.date} onSelect={this.handleFilterChange.bind(this, 'deployments', 'date')} />
+        },
+        {
+          id: 'name',
+          label: <SortHeader id='name' title='Name' sort={this.state.deployments.sort} onClick={this.handleSortChange.bind(this, 'deployments', 'name')} />
+        },
+        { id: 'country', label: 'Country' },
+        {
+          id: 'type',
+          label: <FilterHeader id='type' title='Type' options={deployTypes} filter={this.state.deployments.filters.type} onSelect={this.handleFilterChange.bind(this, 'deployments', 'type')} />
+        },
+        { id: 'personnel', label: 'Number of Personnel', className: 'right-align' }
+      ];
+
+      // const rows = data.objects.map(o => ({
+      //   id: o.id,
+      //   date: DateTime.fromISO(o.created_at).toISODate(),
+      //   name: <Link to={`/reports/${o.id}`} className='link--primary' title='View Field Report'>{o.summary}</Link>,
+      //   event: o.event ? <Link to={`/emergencies/${o.event.id}`} className='link--primary' title='View Emergency'>{o.event.name}</Link> : 'n/a',
+      //   dtype: o.dtype.name,
+      //   countries: <ul>{o.countries.map(country => <li key={country.id}><Link to={`/countries/${country.id}`} className='link--primary' title='View Country'>{country.name}</Link></li>)}</ul>
+      // }));
+      const rows = [
+        {
+          id: 'test',
+          date: DateTime.local().toISODate(),
+          name: 'Test entry',
+          country: 'Portugal',
+          type: 'X',
+          personnel: {
+            value: 1000,
+            className: 'right-align'
+          }
+        }
+      ];
+
+      return (
+        <Fold title={`Current Deployments (${data.meta.total_count})`}>
+          <DisplayTable
+            headings={headings}
+            rows={rows}
+            pageCount={data.meta.total_count / data.meta.limit}
+            page={data.meta.offset / data.meta.limit}
+            onPageChange={this.handlePageChange.bind(this, 'deployments')}
+          />
+        </Fold>
+      );
+    }
+
+    return null;
+  }
+
   renderContent () {
     const {
       fetched,
@@ -107,28 +258,7 @@ class Deployments extends React.Component {
         </div>
         <div className='inpage__body'>
           <div className='inner'>
-            <div className='fold'>
-              <div className='inner'>
-                <div className='fold__header'>
-                  <h2 className='fold__title'>Current Deployments </h2>
-                </div>
-                <div className='fold__body'>
-                  <table className='table table--zebra'>
-                    <thead>
-                      <tr>
-                        <th>Start date</th>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>Type</th>
-                        <th>Number of Personnel</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+            {this.renerDeploymentsTable()}
             <div className='readiness__container'>
               <Readiness eruOwners={this.props.eruOwners} />
             </div>
