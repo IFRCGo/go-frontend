d441c07929571ae7f93cc645c081a314ba8036a9
Derek Lieu <dl@dereklieu.com>
2018-06-19 23:32:07 +0200

    Include FDRS login as env

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -3,6 +3,8 @@
  * App config for production.
  */
 module.exports = {
+  fdrsUser: process.env['fdrs_user'],
+  fdrsPass: process.env['fdrs_pass'],
   environment: 'production',
   api: 'https://prddsgocdnapi.azureedge.net/',
   mbtoken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q'
--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -3,6 +3,8 @@
  * App config overrides for staging.
  */
 module.exports = {
+  fdrsUser: process.env['fdrs_user'],
+  fdrsPass: process.env['fdrs_pass'],
   environment: 'staging',
   api: 'https://dsgocdnapi.azureedge.net/',
   mbtoken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q'
