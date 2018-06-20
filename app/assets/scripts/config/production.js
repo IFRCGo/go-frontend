3089ffb3cb86299624b6f2a7e49094ebf0d7cabd
Derek Lieu <dl@dereklieu.com>
2018-06-20 10:50:51 +0200

    Include fdrs auth as env variables

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -3,8 +3,7 @@
  * App config for production.
  */
 module.exports = {
-  fdrsUser: process.env['fdrs_user'],
-  fdrsPass: process.env['fdrs_pass'],
+  fdrsAuth: process.env['fdrs_auth'],
   environment: 'production',
   api: 'https://prddsgocdnapi.azureedge.net/',
   mbtoken: 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q'
