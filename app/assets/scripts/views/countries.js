e8aeca760b3ec2349e019fccd403751cc8d372db
Derek Lieu <dl@dereklieu.com>
2018-06-26 11:57:49 +0200

    Remove extra export button from country view

--- app/assets/scripts/views/countries.js
+++ app/assets/scripts/views/countries.js
@@ -358,9 +358,6 @@ class AdminArea extends SFPComponent {
             <div className='inner'>
               <KeyFigures data={this.props.keyFigures} />
               <Fold title='Statistics' headerClass='visually-hidden' id='operations'>
-                <div className='fold__actions'>
-                  <button className='button button--primary-bounded'>Export Map</button>
-                </div>
                 <h2 className='fold__title'>{isNaN(activeOperations) ? nope : activeOperations + ' Active Operations'}</h2>
                 <div className='operations__container'>
                   <BulletTable tables={bulletTables} title='PNS Activities' />
