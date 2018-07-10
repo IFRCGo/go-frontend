9977551f4c489d38c719aff08896d8d37d95e8b3
Derek Lieu <dl@dereklieu.com>
2018-07-10 15:34:15 -0400

    Remove duplicative logic to generate export links

--- app/assets/scripts/views/table.js
+++ app/assets/scripts/views/table.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import qs from 'qs';
 
-import { environment, api } from '../config';
+import { environment } from '../config';
 import { regions } from '../utils/region-constants';
 import { getCountryMeta } from '../utils/get-country-meta';
 import { get } from '../utils/utils';
@@ -24,55 +24,6 @@ const displayTypes = {
 };
 
 class Table extends React.Component {
-  getExportLink () {
-    let route = api;
-    switch (this.props.type) {
-      case 'emergency':
-        route += 'api/v2/event/?';
-        break;
-      case 'report':
-        route += 'api/v2/field_report/?';
-        break;
-      case 'appeal':
-        route += 'api/v2/appeal/?';
-        break;
-      case 'alert':
-        route += 'api/v2/surge_alert/?';
-        break;
-    }
-    return route + this.getParsedQueryParams();
-  }
-
-  getParsedQueryParams () {
-    const { type } = this.props;
-    let q = { limit: 100 };
-    const query = this.getQueryParams();
-    if (query.region) {
-      if (type === 'emergency' || type === 'report') {
-        q.regions__in = query.region;
-      } else if (type === 'appeal') {
-        q.region = query.region;
-      }
-    } else if (query.country) {
-      if (type === 'emergency' || type === 'report') {
-        q.countries__in = query.country;
-      } else if (type === 'appeal') {
-        q.country = query.country;
-      }
-    }
-
-    if (query.atype) {
-      q.atype = query.atype === 'appeal' ? '1'
-        : query.atype === 'dref' ? '0' : null;
-    }
-
-    if (query.record && !isNaN(query.record)) {
-      q.id = query.record;
-    }
-
-    return qs.stringify(q);
-  }
-
   getQueryParams () {
     const { search } = this.props.location;
     const query = search ? qs.parse(search.slice(1, search.length)) : {};
@@ -83,7 +34,7 @@ class Table extends React.Component {
     const { type } = this.props;
     let props = {
       limit: 50,
-      exportLink: this.getExportLink()
+      exportLink: true
     };
 
     const query = this.getQueryParams();
