b5ec9bf664c1de13abad15108f5a90f3a385db81
Szabo Zoltan <szabozoltan69@gmail.com>
2018-11-14 17:03:19 +0100

    Change the About text

--- app/assets/scripts/views/about.js
+++ app/assets/scripts/views/about.js
@@ -23,12 +23,13 @@ export default class About extends React.Component {
             <div className='inner'>
               <div className='fold'>
                 <div className='inner'>
-                  <h3>What is Go?</h3>
-                  <p>With Go we aim to create a team of multifunctional information and data professionals within the IFRC who operate with a set of common design principles. This team will support internal change around data services and catalyze collaboration with technology partners, humanitarian organizations, governments, and civil society organizations, individuals and communities on this journey. The team will work with all parts of the IFRC and ICRC where possible to create analyze and share relevant data.</p>
-                  <h3>Go Work Streams</h3>
-                  <p>The Go Team will deliver three major streams of work focusing on i) Data literacy - building the basic organizational foundations of better data services, ii) Single-context situational awareness (small/medium size disaster information) - improving information management within a specific small to medium crisis response including ongoing global overview, and iii) Multi-context complex awareness (major disaster information) - to establish an emergency center that provides Federation leaders with processes, systems and products to increase informational coherence and decision-making during multiple large global crisis response operations.</p>
-                  <h3>Go Products and Services</h3>
-                  <p>Within these work streams, the Go Platform will deliver a suite of solutions that make disaster information universally available and useful for IFRC responders. Examples of these solutions include programs to create good data habits; analytics; data visualizations; trend analysis; hazard mapping; response mapping; gap analysis; and an innovation lab to liaise with and build on existing partnerships with key private sector, United Nations, Non Governmental Organizations.</p>
+                  <p>GO is a Red Cross Red Crescent platform to connect information on emergency needs with the right response.</p>
+                  <p>GO platform video: <a href="https://www.youtube.com/watch?v=dwPsQzla9A4">www.youtube.com/watch?v=dwPsQzla9A4</a></p>
+                  <p>User and administrator guides on <a href="https://drive.google.com/open?id=1F3YQ17AKOvCUvvE4EGgy5pZ0L55_xy2d">Google Drive</a>:
+                    &nbsp;<a href="https://drive.google.com/open?id=1C6PU6wd5KRDpwyLoDcb1GFLWFHyf65Xp"><img src="/assets/graphics/content/usr.jpg" alt="user-guide"/></a> 
+                    &nbsp;<a href="https://drive.google.com/open?id=1Hm5q6hcudJaCSqSIIf0uaMfTboMJlfkL"><img src="/assets/graphics/content/adm.jpg" alt="admin-guide"/></a> 
+                  </p>
+                  <p>For any further information, please contact <a href="mailto:IM@ifrc.org">IM@ifrc.org</a></p>
                   <section className='logo__group'>
                     <p className='logo__title'>Go has received dedicated personnel and funding support from:</p>
                     <ul className='logo__list'>
