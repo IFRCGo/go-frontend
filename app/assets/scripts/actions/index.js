0e1a624e38741fd346d6cdb2b10c049a08597d56
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-03-27 09:22:24 +0100

    RE0304 - Adding featured_event_deployments request to API

--- app/assets/scripts/actions/index.js
+++ app/assets/scripts/actions/index.js
@@ -127,6 +127,11 @@ export function getFeaturedEmergencies () {
   return fetchJSON('/api/v2/event/?is_featured=1', GET_FEATURED_EMERGENCIES, withToken());
 }
 
+export const GET_FEATURED_EMERGENCIES_DEPLOYMENTS = 'GET_FEATURED_EMERGENCIES_DEPLOYMENTS';
+export function getFeaturedEmergenciesDeployments () {
+  return fetchJSON('/api/v2/featured_event_deployments', GET_FEATURED_EMERGENCIES_DEPLOYMENTS, withToken());
+}
+
 export const GET_EMERGENCIES_LIST = 'GET_EMERGENCIES_LIST';
 export function getEmergenciesList (page = 1, filters = {}) {
   filters.limit = filters.limit || 10;
