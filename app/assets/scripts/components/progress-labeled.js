df4749aaddd68b8ce05cab95bd8e9fa1a45c62a6
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-03-27 09:28:07 +0100

    RE0304 - Progress bar max 100% width fix

--- app/assets/scripts/components/progress-labeled.js
+++ app/assets/scripts/components/progress-labeled.js
@@ -3,15 +3,16 @@
 import React from 'react';
 import { PropTypes as T } from 'prop-types';
 import { environment } from '../config';
+import { round } from '../utils/format';
 
 const Progress = ({max, value, children}) => {
   let percent = value / max * 100;
-  percent = percent > 100 ? 100 : percent;
+  percent = round(percent);
 
   return (
     <div className='progress-bar-container'>
       <div className='progress-bar'>
-        <div className='progress-bar__value' style={{width: percent + '%'}}></div>
+        <div className='progress-bar__value' style={{width: (percent > 100 ? 100 : percent) + '%'}}></div>
       </div>
       <div className='progress-bar__key'><small>{percent}%</small></div>
     </div>
