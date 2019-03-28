d6213c37e35fbc733c3337ef581d36070674d407
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-03-28 13:37:40 +0100

    RE0304 - Progressbar: Coverage can go above 100, bar not

--- app/assets/scripts/components/progress-labeled.js
+++ app/assets/scripts/components/progress-labeled.js
@@ -6,15 +6,12 @@ import { environment } from '../config';
 import { round } from '../utils/format';
 
 const Progress = ({max, value, children}) => {
-  let percent = value / max * 100;
-  percent = round(percent);
-
   return (
     <div className='progress-bar-container'>
       <div className='progress-bar'>
-        <div className='progress-bar__value' style={{width: (percent > 100 ? 100 : percent) + '%'}}></div>
+        <div className='progress-bar__value' style={{width: (value > max ? max : value) + '%'}}></div>
       </div>
-      <div className='progress-bar__key'><small>{percent}%</small></div>
+      <div className='progress-bar__key'><small>{value}%<br/>coverage</small></div>
     </div>
   );
 };
