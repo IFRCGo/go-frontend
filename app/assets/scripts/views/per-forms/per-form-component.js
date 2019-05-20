6a247897a3556658caec280ebe2e0c04472fcdb5
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-05-20 10:27:49 +0200

    PER - Refactor performs to Container - Component pattern

---
+++ app/assets/scripts/views/per-forms/per-form-component.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+const renderLanguageSelectDropdown = (props) => {
+  return (<select onChange={props.chooseLanguage}>
+    <option value='english'>English</option>
+    <option value='spanish'>Spanish</option>
+    <option value='french'>French</option>
+  </select>);
+};
+
+const renderFormTitle = (props) => {
+  return (
+    <div className="fold__header">
+      <h2 className="fold__title">{props.state.title}</h2>
+    </div>
+  );
+};
+
+const renderEpidemicsRadioButton = (props) => {
+  return (
+    <React.Fragment>
+      <div className='per_form_area'>{props.state.areaTitle}</div>
+      <div className='per_form_question'>{props.state.areaQuestion}</div>
+      <input type='radio' name='a1' value={props.state.areaOptions[0]} onClick={props.changeEpiComponentState} /> {props.state.areaOptions[0]} <br />
+      <input type='radio' name='a1' value={props.state.areaOptions[1]} onClick={props.changeEpiComponentState} /> {props.state.areaOptions[1]}
+    </React.Fragment>
+  );
+};
+
+const renderComponents = (props) => {
+  const components = [];
+  let componentIndex = 0;
+
+  console.log(props);
+  props.state.components.forEach(component => {
+    components.push(<React.Fragment>
+      <div className='per_form_area'>{component.componentTitle}</div>
+      {component.componentDescription}<br /><br />
+
+      {renderQuestions(component, componentIndex)}
+      {renderEpiComponent(props, componentIndex)}
+    </React.Fragment>);
+    componentIndex++;
+  });
+
+  return components;
+};
+
+const renderQuestions = (component, componentIndex) => {
+  const questions = [];
+  let questionIndex = 0;
+
+  if (typeof component.namespaces !== 'undefined' && component.namespaces !== null) {
+    component.namespaces.forEach(namespace => {
+      questions.push(<React.Fragment>
+        <div className='per_form_ns'>{namespace.nsTitle}</div>
+        <div className='per_form_question'>{namespace.nsQuestion}</div>
+
+        {renderAnswers(namespace, componentIndex, questionIndex)}
+
+        <div className='per_form_question'>{namespace.feedbackTitle}</div>
+        {typeof namespace.feedbackDescription !== 'undefined' && namespace.feedbackDescription !== null && namespace.feedbackDescription.trim() !== '' ? (<React.Fragment>{namespace.feedbackDescription}<br /></React.Fragment>) : null}
+        <input type='text' name={'q' + componentIndex + 'f'} /><br /><br />
+      </React.Fragment>);
+      questionIndex++;
+    });
+  }
+
+  return questions;
+};
+
+const renderAnswers = (namespace, componentIndex, questionIndex) => {
+  const answers = [];
+  namespace.nsAnswers.forEach(answer => {
+    answers.push(<React.Fragment>
+      <input type='radio' name={'q' + componentIndex + '' + questionIndex} value={answer} /> {answer}<br />
+    </React.Fragment>);
+  });
+  return answers;
+};
+
+const renderEpiComponent = (props, namespaceIndex) => {
+  if (props.epiComponent === 'yes') {
+    return (<React.Fragment>
+      <div className='per_form_ns'>Epidemic preparedess</div>
+      <div>
+        Please take into consideration the following aspects:<br />
+        - The national public health emergency response strategy/plan outlines NS role in epidemics prevention, detection and response.<br />
+        - NS Mandate in epidemics is understood by staff.<br />
+        - The National Epidemic Strategy/Plan adheres to International Health Regulations.<br /><br />
+      </div>
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='Not Reviewed' /> Not Reviewed<br />
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='Does not exist' /> Does not exist<br />
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='Partially exists' /> Partially exists<br />
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='Need improvements' /> Need improvements<br />
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='Exists, could be strengthened' /> Exists, could be strengthened<br />
+      <input type='radio' name={'q' + namespaceIndex + 'epi'} value='High performance' /> High performance<br />
+    </React.Fragment>);
+  }
+};
+
+export const PerFormComponent = (props) => {
+  return (
+    <div className='fold'>
+      <div className='inner'>
+        {renderLanguageSelectDropdown(props)}
+        {renderFormTitle(props)}
+        {renderEpidemicsRadioButton(props)}
+        {renderComponents(props)}
+
+        <input type='checkbox' name='draft' value='yes' /> Save as Draft<br />
+        <button onClick={props.sendForm}>Submit</button>
+      </div>
+    </div>
+  );
+};
--- app/assets/scripts/views/per-forms/per-form.js
+++ app/assets/scripts/views/per-forms/per-form.js
@@ -1,66 +1,20 @@
 import React from 'react';
+import { PerFormComponent } from './per-form-component';
 import RequestFactory from './factory/request-factory';
 
 export default class PerForm extends React.Component {
   constructor (props) {
     super(props);
-    this.composeForms = this.composeForms.bind(this);
-    this.composeNamespaces = this.composeNamespaces.bind(this);
-    this.composeAnswers = this.composeAnswers.bind(this);
+    this.state = {epiComponent: 'no'};
     this.sendForm = this.sendForm.bind(this);
     this.saveDraft = this.saveDraft.bind(this);
     this.loadDraft = this.loadDraft.bind(this);
+    this.changeEpiComponentState = this.changeEpiComponentState.bind(this);
     this.requestFactory = new RequestFactory();
   }
 
   componentDidMount() {
-    this.loadDraft();
-  }
-
-  composeForms () {
-    const forms = [];
-
-    this.state.components.forEach(component => {
-      forms.push(<React.Fragment>
-        <div className='per_form_area'>{component.componentTitle}</div>
-        {component.componentDescription}<br /><br />
-        {this.composeNamespaces(component)}
-      </React.Fragment>);
-    });
-
-    return forms;
-  }
-
-  composeNamespaces (component) {
-    const namespaces = [];
-    let namespaceIndex = 0;
-
-    if (typeof component.namespaces !== 'undefined' && component.namespaces !== null) {
-      component.namespaces.forEach(namespace => {
-        namespaces.push(<React.Fragment>
-          <div className='per_form_ns'>{namespace.nsTitle}</div>
-          <div className='per_form_question'>{namespace.nsQuestion}</div>
-          {this.composeAnswers(namespace, namespaceIndex)}
-          <div className='per_form_question'>{namespace.feedbackTitle}</div>
-          {typeof namespace.feedbackDescription !== 'undefined' && namespace.feedbackDescription !== null && namespace.feedbackDescription.trim() !== '' ? (<React.Fragment>{namespace.feedbackDescription}<br /></React.Fragment>) : null}
-          <input type='text' name={'q' + namespaceIndex + 'f'} /><br /><br />
-        </React.Fragment>);
-        namespaceIndex++;
-      });
-    }
-
-    return namespaces;
-  }
-
-  composeAnswers (namespace, namespaceIndex) {
-    const answers = [];
-    namespace.nsAnswers.forEach(answer => {
-      answers.push(<React.Fragment>
-        <input type='radio' name={'q' + namespaceIndex} value={answer} /> {answer}<br />
-      </React.Fragment>);
-    });
-
-    return answers;
+    //this.loadDraft();
   }
 
   saveDraft () {
@@ -91,7 +45,6 @@ export default class PerForm extends React.Component {
     let request = this.requestFactory.newFormRequest(this.formCode, this.formName, this.state.languageCode);
     request = this.requestFactory.addAreaQuestionData(request);
     request = this.requestFactory.addComponentData(request);
-    console.log(request);
     // fetch('https://dsgocdnapi.azureedge.net/sendperform', {
     //   method: 'POST',
     //   headers: {
@@ -102,32 +55,14 @@ export default class PerForm extends React.Component {
     // });
   }
 
+  changeEpiComponentState (e) {
+    this.setState({epiComponent: e.target.value});
+  }
+
   render () {
-    return (
-      <div className='fold'>
-        <div className='inner'>
-          <select onChange={this.chooseLanguage}>
-            <option value='english'>English</option>
-            <option value='spanish'>Spanish</option>
-            <option value='french'>French</option>
-          </select>
-
-          <div className="fold__header">
-            <h2 className="fold__title">{this.state.title}</h2>
-          </div>
-
-          <div className='per_form_area'>{this.state.areaTitle}</div>
-          <div className='per_form_question'>{this.state.areaQuestion}</div>
-          <input type='radio' name='a1' value={this.state.areaOptions[0]} /> {this.state.areaOptions[0]} <br />
-          <input type='radio' name='a1' value={this.state.areaOptions[1]} /> {this.state.areaOptions[1]}
-
-          {this.composeForms()}
-
-          <input type='checkbox' name='draft' value='yes' /> Save as Draft<br />
-          <button onClick={this.sendForm}>Submit</button>
-
-        </div>
-      </div>
-    );
+    return <PerFormComponent chooseLanguage={this.chooseLanguage}
+      changeEpiComponentState={this.changeEpiComponentState}
+      sendForm={this.sendForm}
+      state={this.state} />;
   }
 }
