871df430597ea80a83d395e0d9618fd8eba5f9d8
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-07-02 16:29:34 +0200

    PERV - Preparedness data fetch and building BETA

--- app/assets/scripts/views/preparedness.js
+++ app/assets/scripts/views/preparedness.js
@@ -3,17 +3,66 @@ import App from './app';
 import PreparednessHeader from './../components/preparedness-header';
 import PerMap from './../components/map/per-map';
 import { connect } from 'react-redux';
-import { getAppealsList } from './../actions';
+import {
+  getAppealsList,
+  getCollaboratingPerCountry
+} from './../actions';
 import { Helmet } from 'react-helmet';
 import { PropTypes as T } from 'prop-types';
 import { environment } from '../config';
+import { getCountryMeta } from './../utils/get-country-meta';
+import { getCentroid } from './../utils/country-centroids';
+import _groupBy from 'lodash.groupby';
 
 class Preparedness extends React.Component {
+  constructor (props) {
+    super(props);
+    this.geoJsonFinal = null;
+  }
+
   componentDidMount () {
     this.props._getAppealsList();
+    this.props._getCollaboratingPerCountry();
+  }
+
+  componentDidUpdate (prevProps) {
+    // console.log('ComponentDidUpdate');
+    // console.log(prevProps);
+    // console.log(this.props);
+    if (prevProps.collaboratingPerCountry.receivedAt !== this.props.collaboratingPerCountry.receivedAt) {
+      const geoJson = {
+        type: 'FeatureCollection',
+        features: []
+      };
+
+      this.props.collaboratingPerCountry.data.results.map((perForm) => {
+        let countryMeta = getCountryMeta(perForm.country.id);
+        perForm.country.iso = countryMeta.iso;
+        let countryCentroid = getCentroid(perForm.country.iso);
+        perForm.country.centroid = countryCentroid;
+
+        geoJson.features.push({
+          geometry: {
+            type: 'Point',
+            coordinates: countryCentroid
+          },
+          properties: perForm
+        });
+
+        return perForm;
+      });
+      // console.log(geoJson);
+      this.geoJsonFinal = {error: false, fetched: true, fetching: false, receivedAt: false, data: {geoJSON: geoJson}};
+      const grouped = _groupBy(this.props.collaboratingPerCountry.data.results.filter(perForm => perForm.country), 'country.iso');
+      // console.log('GROUPED');
+      // console.log(grouped);
+    }
   }
 
   render () {
+    console.log('*** RENDER ***');
+    console.log(this.geoJsonFinal);
+    console.log(this.props.appealsList);
     return (
       <App className='page--homepage'>
         <section className='inpage'>
@@ -33,7 +82,7 @@ class Preparedness extends React.Component {
           </header>
           <div className='inpage__body'>
             <PreparednessHeader />
-            <PerMap operations={this.props.appealsList} noExport={true} noRenderEmergencies={true} />
+            { this.geoJsonFinal !== null ? <PerMap operations={this.geoJsonFinal} noExport={true} noRenderEmergencies={true} /> : null }
           </div>
         </section>
       </App>);
@@ -51,11 +100,13 @@ if (environment !== 'production') {
 }
 
 const selector = (state) => ({
-  appealsList: state.overallStats.appealsList
+  appealsList: state.overallStats.appealsList,
+  collaboratingPerCountry: state.perForm.getCollaboratingPerCountry
 });
 
 const dispatcher = (dispatch) => ({
-  _getAppealsList: (...args) => dispatch(getAppealsList(...args))
+  _getAppealsList: (...args) => dispatch(getAppealsList(...args)),
+  _getCollaboratingPerCountry: () => dispatch(getCollaboratingPerCountry())
 });
 
 export default connect(selector, dispatcher)(Preparedness);
