528f1a1f12d3bb668c080039522db733a69c0ac4
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-07-04 15:03:36 +0200

    PERV - adding ns engaged per graphs to preparedness

--- app/assets/scripts/views/preparedness.js
+++ app/assets/scripts/views/preparedness.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import App from './app';
-import PreparednessHeader from './../components/preparedness-header';
+import PreparednessHeader from '../components/preparedness/preparedness-header';
 import PerMap from './../components/map/per-map';
 import { connect } from 'react-redux';
-import { getCollaboratingPerCountry } from './../actions';
+import { getCollaboratingPerCountry, getPerEngagedNsPercentage } from './../actions';
 import { Helmet } from 'react-helmet';
 import { PropTypes as T } from 'prop-types';
 import { environment } from '../config';
 import { getCountryMeta } from './../utils/get-country-meta';
 import { getCentroid } from './../utils/country-centroids';
+import NationalSocietiesEngagedPer from '../components/preparedness/national-societies-engaged-per';
 // import _groupBy from 'lodash.groupby';
 
 class Preparedness extends React.Component {
@@ -19,6 +20,7 @@ class Preparedness extends React.Component {
 
   componentDidMount () {
     this.props._getCollaboratingPerCountry();
+    this.props._getPerEngagedNsPercentage();
   }
 
   componentDidUpdate (prevProps) {
@@ -71,6 +73,7 @@ class Preparedness extends React.Component {
           <div className='inpage__body'>
             <PreparednessHeader />
             { this.geoJsonFinal !== null ? <PerMap data={this.geoJsonFinal} noExport={true} noRenderEmergencies={true} /> : null }
+            { this.props.getPerEngagedNsPercentage.fetched ? <NationalSocietiesEngagedPer data={this.props.getPerEngagedNsPercentage} /> : null }
           </div>
         </section>
       </App>);
@@ -83,16 +86,20 @@ class Preparedness extends React.Component {
 if (environment !== 'production') {
   Preparedness.propTypes = {
     _getCollaboratingPerCountry: T.func,
+    _getPerEngagedNsPercentage: T.func,
+    getPerEngagedNsPercentage: T.object,
     collaboratingPerCountry: T.object
   };
 }
 
 const selector = (state) => ({
-  collaboratingPerCountry: state.perForm.getCollaboratingPerCountry
+  collaboratingPerCountry: state.perForm.getCollaboratingPerCountry,
+  getPerEngagedNsPercentage: state.perForm.getPerEngagedNsPercentage
 });
 
 const dispatcher = (dispatch) => ({
-  _getCollaboratingPerCountry: () => dispatch(getCollaboratingPerCountry())
+  _getCollaboratingPerCountry: () => dispatch(getCollaboratingPerCountry()),
+  _getPerEngagedNsPercentage: () => dispatch(getPerEngagedNsPercentage())
 });
 
 export default connect(selector, dispatcher)(Preparedness);
