c72c9d5992a45aafa8d3af419b60c5b17300dba1
Mark Mano Molnar <markmanomolnar@gmail.com>
2019-07-09 13:03:23 +0200

    PERV - Adding phase to per-map popover

--- app/assets/scripts/components/map/per-map/operations-popover.js
+++ app/assets/scripts/components/map/per-map/operations-popover.js
@@ -9,7 +9,7 @@ import React from 'react';
 
 class OperationsPopover extends React.Component {
   render () {
-    const { pageId, navigate, title, onCloseClick, deployments } = this.props;
+    const { pageId, navigate, title, onCloseClick, deployments, phase } = this.props;
     return (
       <article className='popover'>
         <div className='popover__contents'>
@@ -26,14 +26,57 @@ class OperationsPopover extends React.Component {
             </div>
           </header>
           <div className='popover__body'>
-            <div style={{width: '50%', float: 'left'}}>
+            <div style={{width: '50%', float: 'left', fontWeight: 'bold', marginBottom: '10px'}}>
               current per process phase
             </div>
-            <div style={{width: '50%', float: 'left'}}>
+            <div style={{width: '50%', float: 'left', fontWeight: 'bold', marginBottom: '10px'}}>
               current per process type
             </div>
             <div style={{width: '50%', float: 'left'}}>
-              *** PHASE ****
+              <div style={{marginBottom: '5px', float: 'left', width: '100%'}}>
+                <div style={{width: '80%', float: 'left'}}>
+                  Orientation
+                </div>
+                <div style={{width: '20%', float: 'left'}}>
+                  {phase.phase >= 0 ? <img src='/assets/graphics/layout/tick.png' alt='phase ticked' /> : null}
+                </div>
+              </div>
+
+              <div style={{marginBottom: '5px', float: 'left', width: '100%'}}>
+                <div style={{width: '80%', float: 'left'}}>
+                  Assessment
+                </div>
+                <div style={{width: '20%', float: 'left'}}>
+                  {phase.phase >= 1 ? <img src='/assets/graphics/layout/tick.png' alt='phase ticked' /> : null}
+                </div>
+              </div>
+
+              <div style={{marginBottom: '5px', float: 'left', width: '100%'}}>
+                <div style={{width: '80%', float: 'left'}}>
+                  Prioritization
+                </div>
+                <div style={{width: '20%', float: 'left'}}>
+                  {phase.phase >= 2 ? <img src='/assets/graphics/layout/tick.png' alt='phase ticked' /> : null}
+                </div>
+              </div>
+
+              <div style={{marginBottom: '5px', float: 'left', width: '100%'}}>
+                <div style={{width: '80%', float: 'left'}}>
+                  Plan of action
+                </div>
+                <div style={{width: '20%', float: 'left'}}>
+                  {phase.phase >= 3 ? <img src='/assets/graphics/layout/tick.png' alt='phase ticked' /> : null}
+                </div>
+              </div>
+
+              <div style={{marginBottom: '5px', float: 'left', width: '100%'}}>
+                <div style={{width: '80%', float: 'left'}}>
+                  Action &amp; Accountability
+                </div>
+                <div style={{width: '20%', float: 'left'}}>
+                  {phase.phase >= 4 ? <img src='/assets/graphics/layout/tick.png' alt='phase ticked' /> : null}
+                </div>
+              </div>
             </div>
             <div style={{width: '50%', float: 'left'}}>
               *** TYPE ***
