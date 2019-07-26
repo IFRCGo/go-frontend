3b805caca3b88afa64acacffb5e72081a4d6f886
Sanjay Bhangar <sanjaybhangar@gmail.com>
2019-07-26 23:39:11 +0530

    hide Drop autosave button from PER forms

--- app/assets/scripts/components/per-forms/per-form-component.js
+++ app/assets/scripts/components/per-forms/per-form-component.js
@@ -298,7 +298,8 @@ export const PerFormComponent = (props) => {
   return (
     <React.Fragment>
       <Link to='/account#per-forms' className='button button--medium button--primary-filled' style={{float: 'right', marginBottom: '1rem'}}>Exit form</Link>
-      {props.view ? null : <button className='button button--medium button--secondary-filled' id={'autosave' + props.mode + props.formCode + props.ns} onClick={dropAutosaveAndReload} style={{float: 'right', marginBottom: '1rem', marginRight: '1rem'}}>Drop autosave &amp; reload draft</button>}
+      {/* Commenting out Drop Autosave button because we want to hide it for now, we may move it to somewhere more hidden. */}
+      {/* {props.view ? null : <button className='button button--medium button--secondary-filled' id={'autosave' + props.mode + props.formCode + props.ns} onClick={dropAutosaveAndReload} style={{float: 'right', marginBottom: '1rem', marginRight: '1rem'}}>Drop autosave &amp; reload draft</button>} */}
       <div className='fold'>
         <div className='inner'>
           {renderLanguageSelectDropdown(props)}
