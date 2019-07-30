bcec055779a717f83799fba28e60ca6c9d147d3d
Necoline <hnhubner@gmail.com>
2019-07-30 11:50:36 -0400

    adds error panel

---
+++ app/assets/scripts/components/errorPanel.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+"use strict";
+
+import React from "react";
+import { PropTypes as T } from "prop-types";
+import { environment } from "../config";
+
+import Fold from "./fold";
+
+const ErrorPanel = props => {
+  return (
+    <Fold title={props.title} id="errorMessage">
+      <span>{props.errorMessage}</span>
+    </Fold>
+  );
+};
+
+if (environment !== "production") {
+  ErrorPanel.propTypes = {
+    title: T.string,
+    errorMessage: T.string
+  };
+}
+
+export default ErrorPanel;
