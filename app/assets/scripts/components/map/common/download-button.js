23fdb832db3624046a8098ab1dd927a782cf2905
Sanjay B <sanjaybhangar@gmail.com>
2019-08-26 19:41:39 +0530

    enable download map button on all browsers, fix for firefox

--- app/assets/scripts/components/map/common/download-button.js
+++ app/assets/scripts/components/map/common/download-button.js
@@ -14,66 +14,67 @@ class DownloadButton extends React.Component {
   }
 
   startDownload () {
-    if ((/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) || /Trident/.test(navigator.userAgent) || /Edge/.test(navigator.userAgent)) {
-      this.props.setZoomToDefault();
 
-      const interval = setInterval(function () {
-        clearInterval(interval);
-        const timestamp = new Date();
-        const map = document.getElementsByClassName('map-vis')[0];
-        const downloadButton = document.getElementsByClassName('map-vis__legend--download-btn')[0];
-        const dropdowns = Array.from(document.getElementsByClassName('map-vis__legend--top-left'));
-        const popover = document.getElementsByClassName('popover__contents')[0];
-        const navigation = document.getElementsByClassName('mapboxgl-ctrl-top-right')[0];
-        const mapLogoHeader = document.getElementById('map-picture-header');
+    this.props.setZoomToDefault();
+    const interval = setInterval(function () {
+      clearInterval(interval);
+      const timestamp = new Date();
+      const map = document.getElementsByClassName('map-vis')[0];
+      const downloadButton = document.getElementsByClassName('map-vis__legend--download-btn')[0];
+      const dropdowns = Array.from(document.getElementsByClassName('map-vis__legend--top-left'));
+      const popover = document.getElementsByClassName('popover__contents')[0];
+      const navigation = document.getElementsByClassName('mapboxgl-ctrl-top-right')[0];
+      const mapLogoHeader = document.getElementById('map-picture-header');
+    
+      const $canvas = document.getElementsByClassName('mapboxgl-canvas')[0];
+      const $expimg = document.getElementById('exportimage');
+      $expimg.src = $canvas.toDataURL('png');
+      $expimg.style.display = 'block';
+      document.getElementsByClassName('mapboxgl-map')[0].style.visibility = 'hidden';
+      
+      mapLogoHeader.style.visibility = 'visible';
+      downloadButton.style.visibility = 'hidden';
+      navigation.style.visibility = 'hidden';
+      dropdowns.forEach(dropdown => {
+        dropdown.style.visibility = 'hidden';
+      });
 
-        mapLogoHeader.style.visibility = 'visible';
-        downloadButton.style.visibility = 'hidden';
-        navigation.style.visibility = 'hidden';
+      if (typeof popover !== 'undefined') {
+        popover.style.height = 'fit-content';
+        popover.style.maxHeight = 'none';
+      }
+
+      html2canvas(map, {useCORS: true}).then((renderedCanvas) => {
+        startDownload(
+          renderedCanvas,
+          'map-' + timestamp.getTime() + '.png');
+
+        mapLogoHeader.style.visibility = 'hidden';
+        downloadButton.style.visibility = 'visible';
+        navigation.style.visibility = 'visible';
         dropdowns.forEach(dropdown => {
-          dropdown.style.visibility = 'hidden';
+          dropdown.style.visibility = 'visible';
         });
+        
+        $expimg.style.display = 'none';
+        document.getElementsByClassName('mapboxgl-map')[0].style.visibility = 'visible';
 
         if (typeof popover !== 'undefined') {
-          popover.style.height = 'fit-content';
-          popover.style.maxHeight = 'none';
+          popover.style.height = 'auto';
+          popover.style.maxHeight = '225px';
         }
-
-        html2canvas(map, {useCORS: true}).then((renderedCanvas) => {
-          startDownload(
-            renderedCanvas,
-            'map-' + timestamp.getTime() + '.png');
-
-          mapLogoHeader.style.visibility = 'hidden';
-          downloadButton.style.visibility = 'visible';
-          navigation.style.visibility = 'visible';
-          dropdowns.forEach(dropdown => {
-            dropdown.style.visibility = 'visible';
-          });
-
-          if (typeof popover !== 'undefined') {
-            popover.style.height = 'auto';
-            popover.style.maxHeight = '225px';
-          }
-        });
-      }, 1000);
-    } else {
-      showAlert('danger', <p><b>Error:</b> Download is only possible from Google Chrome</p>, true, 2000);
-    }
+      });
+    }, 1000);
   }
 
   render () {
-    if ((/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) || /Trident/.test(navigator.userAgent) || /Edge/.test(navigator.userAgent)) {
-      return (
-        <figcaption
-          className='map-vis__legend map-vis__legend--download-btn legend'
-          onClick={this.startDownload}>
-
-          <img src='/assets/graphics/content/download.svg' alt='IFRC GO logo'/>
-        </figcaption>
-      );
-    }
-    return null;
+    return (
+      <figcaption
+        className='map-vis__legend map-vis__legend--download-btn legend'
+        onClick={this.startDownload}>
+        <img src='/assets/graphics/content/download.svg' alt='IFRC GO logo'/>
+      </figcaption>
+    );
   }
 }
 
