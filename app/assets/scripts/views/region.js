f0036819040c8180560c1a78b1dee7fd716399bc
Sanjay B <sanjaybhangar@gmail.com>
2019-09-02 20:59:08 +0530

    fix display of alphabetical country list on region page, toward #774

--- app/assets/scripts/views/region.js
+++ app/assets/scripts/views/region.js
@@ -286,15 +286,36 @@ class AdminArea extends SFPComponent {
         return Object.assign({ numOperations }, d);
       });
     }
+
+    // Create the <li> elements for the list
+    // This code can probably be improved,
+    // The idea here is to generate an array of <li> elements with
+    // the first letter like 'C', 'D', etc. being array items, with a different class.
+    //  It seemed hard to style a flowing 3-column list if this is broken up into separate <ul>s
+    let countryItems = [];
+    let currLetter = 'A';
+    countryItems.push(
+      <li className='region-countries__letter' key={currLetter}>{currLetter}</li>
+    );
+    countries.forEach((country, idx) => {
+      const name = country.name;
+      if (name[0] !== currLetter) {
+        currLetter = name[0];
+        countryItems.push(
+          <li className='region-countries__letter' key={currLetter}>{currLetter}</li>
+        );
+      }
+      countryItems.push(
+        <li key={country.id} className='region-countries__item'>
+          <Link to={`/countries/${country.id}`} className='region-countries__link'><span className='region-countries__linkC'>{country.name}</span></Link>
+          {country.numOperations ? <span className='region-countries__link-op'>({country.numOperations} Active Operation{country.numOperations > 1 ? 's' : ''})</span> : null}
+        </li>
+      );
+    });
     return (
       <Fold title={countries.length + ' Countries in this Region'}>
         <ul className='region-countries__list'>
-          {countries.map(d => (
-            <li key={d.id} className='region-countries__item'>
-              <Link to={`/countries/${d.id}`} className='link--primary'>{d.name}</Link>
-              {d.numOperations ? <span><strong>{d.numOperations}</strong> Active Operation{d.numOperations > 1 ? 's' : ''}</span> : null}
-            </li>
-          ))}
+          {countryItems}
         </ul>
       </Fold>
     );
