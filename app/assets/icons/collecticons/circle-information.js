e197d19ace85c32ddeb2df204404549f8110af35
Necoline <necoline@developmentseed.org>
2019-10-21 14:50:36 -0400

    shrinks tooltip icon

--- app/assets/icons/collecticons/circle-information.js
+++ app/assets/icons/collecticons/circle-information.js
@@ -6,13 +6,13 @@ const InfoIcon = () => {
       version='1.1'
       xmlns='http://www.w3.org/2000/svg'
       xmlnsxlink='http://www.w3.org/1999/xlink'
-      width='16'
-      height='16'
+      width='12'
+      height='12'
       viewBox='0 0 16 16'
     >
       <rect
-        width='16'
-        height='16'
+        width='12'
+        height='12'
         id='icon-bound'
         fill='none'
       />
