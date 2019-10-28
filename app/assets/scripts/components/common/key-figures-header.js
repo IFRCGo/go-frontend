3f9b70f6bee07f8b57e05cc8b943e8f93d88b2ac
Necoline <necoline@developmentseed.org>
2019-10-28 15:59:54 +0100

    dynamically adds icons

--- app/assets/scripts/components/common/key-figures-header.js
+++ app/assets/scripts/components/common/key-figures-header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import c from 'classnames';
 import { percent, shortenLargeNumber } from '../../utils/format';
 import BlockLoading from '../block-loading';
 import { environment } from '../../config';
@@ -20,6 +21,16 @@ const keyTitle = {
   amountFunded: 'Funding (CHF)'
 };
 
+const keyIcon = {
+  activeDrefs: 'collecticon-rc',
+  activeAppeals: 'collecticon-rc-appeals',
+  budget: 'collecticon-cash-notes',
+  appealsFunding: 'collecticon-cash-bag',
+  targetPop: 'collecticon-people-arrows',
+  numBeneficiaries: 'collecticon-people-arrows',
+  amountRequested: 'collecticon-cash-notes',
+  amountFunded: 'collecticon-cash-bag'
+};
 // Lists two tooltip descriptions currently in use.
 const tooltipOptions = {
   activeDrefs: {
@@ -69,6 +80,7 @@ export default function KeyFiguresHeader (props) {
         id: stat,
         title: keyTitle[stat],
         value,
+        icon: keyIcon[stat],
         tooltip: tooltipOptions[stat] || null
       };
     }).filter(figure => props.keyFiguresList.includes(figure.id));
@@ -84,7 +96,7 @@ export default function KeyFiguresHeader (props) {
         <ul className='sumstats'>
           {filteredKeyFigures().map(keyFigure => (
             <li key={keyFigure.id} className='sumstats__item'>
-              <span className='collecticon-rc sumstats__icon'></span>
+              <span className={c(`${keyFigure.icon}`, 'sumstats__icon')}></span>
               <span className='sumstats__value'>{keyFigure.value}</span>
               <span className='sumstats__key'>
                 {keyFigure.title}
