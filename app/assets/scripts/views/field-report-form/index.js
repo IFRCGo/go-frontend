1f644e766a19618199892ad25b302d1691942ce2
Sanjay B <sanjaybhangar@gmail.com>
2019-12-09 14:02:48 +0530

    changes as per https://github.com/IFRCGo/go-frontend/issues/833#issuecomment-562146655

--- app/assets/scripts/views/field-report-form/index.js
+++ app/assets/scripts/views/field-report-form/index.js
@@ -348,6 +348,25 @@ class FieldReportForm extends React.Component {
             property='summary'
           />
         </FormInputSelect>
+        <div className='form__group'>
+          <div className='form__inner-header'>
+            <label className='form__label'>{fields['disaster-type'][status].label}</label>
+          </div>
+          <div className='form__inner-body'>
+            <Select
+              placeholder='Select a disaster type'
+              name='disaster-type'
+              id='disaster-type'
+              options={formData.disasterType}
+              value={this.state.data.disasterType}
+              onChange={this.onFieldChange.bind(this, 'disasterType')}
+            />
+            <FormError
+              errors={this.state.errors}
+              property='disasterType'
+            />
+          </div>
+        </div>
         <FormInput
           label={fields.startDate[status].label}
           type='date'
@@ -398,25 +417,6 @@ class FieldReportForm extends React.Component {
                 property='districts'
               />
             </div>
-          </div>
-        </div>
-        <div className='form__group'>
-          <div className='form__inner-header'>
-            <label className='form__label'>{fields['disaster-type'][status].label}</label>
-          </div>
-          <div className='form__inner-body'>
-            <Select
-              placeholder='Select a disaster type'
-              name='disaster-type'
-              id='disaster-type'
-              options={formData.disasterType}
-              value={this.state.data.disasterType}
-              onChange={this.onFieldChange.bind(this, 'disasterType')}
-            />
-            <FormError
-              errors={this.state.errors}
-              property='disasterType'
-            />
           </div>
         </div>
         <FormRadioGroup
@@ -475,6 +475,7 @@ class FieldReportForm extends React.Component {
             fields.situationFields[status].map(field => {
               return (
                 <SourceEstimation
+                  estimationLabel={field.estimationLabel}
                   label={field.label}
                   description={field.desc}
                   name={field.name}
@@ -592,7 +593,7 @@ class FieldReportForm extends React.Component {
         </React.Fragment>
         <FormRadioGroup
           label='Information Bulletin'
-          description='Indicate if an Information Bulletin was published, is planned or if no Information Bulletin will be issued for this operation/disaster.'
+          description='Indicate if an Information Bulletin was published, is planned or if no Information Bulletin will be issued for this operation/disaster/hazard.'
           name='bulletin'
           options={[
             {
