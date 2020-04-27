fe896ed022a36637516161459245b6e7280fa917
Sanjay B <sanjaybhangar@gmail.com>
2020-04-27 21:18:36 +0530

    fix text on covid-19 banner, refs https://github.com/IFRCGo/go-frontend/issues/1007#issuecomment-619991134

--- app/assets/scripts/components/global-header-banner.js
+++ app/assets/scripts/components/global-header-banner.js
@@ -5,7 +5,7 @@ class GlobalHeaderBanner extends React.PureComponent {
   render () {
     return (
       <div className='global__banner global__banner--danger text-center'>
-        <span>You may find more information related to the ongoing COVID-19 Global Operation by clicking </span>
+        <span>COVID-19 global situational information and technical guidance available </span>
         <a href='https://go.ifrc.org/emergencies/3972'>here.</a>
       </div>
     );
