d057c1dbd478292e08d14bcda2d65be7f00bb21f
frozenhelium <fren.ankit@gmail.com>
2020-05-12 11:13:15 +0545

    Update README

--- src/root/config.js
+++ src/root/config.js
@@ -1,7 +1,8 @@
-export const environment = 'development';
-export const api = process.env.REACT_APP_API_ENDPOINT;
-export const fdrsAuth = process.env.REACT_APP_FDRS_AUTH;
-export const mbtoken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+export const environment = process.env.REACT_APP_ENVIRONMENT || process.env.NODE_ENV || 'development';
+export const api = process.env.REACT_APP_API_ENDPOINT ||
+  process.env.NODE_ENV === 'production' ? 'https://prddsgocdnapi.azureedge.net/' : 'https://dsgocdnapi.azureedge.net/';
+export const fdrsAuth = process.env.REACT_APP_FDRS_AUTH || process.env['fdrsAuth'] || '';
+export const mbtoken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN || 'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg';
 
 const config = {
   api,
