c8c7eae5f32ed42b6f922912340560a5b39c9b41
frozenhelium <fren.ankit@gmail.com>
2020-05-17 11:52:25 +0545

    Add absolute import support for js files

---
+++ .babelrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "presets": ["react-app"],
+  "plugins": [
+    [
+      "module-resolver",
+      {
+        "root": ["."],
+        "extensions": [".js", ".jsx", ".ts", ".tsx"],
+        "alias": {
+          "#actions": "./src/root/actions",
+          "#components": "./src/root/components",
+          "#config": "./src/config",
+          "#hooks": "./src/root/hooks",
+          "#lang": "./src/lang",
+          "#selectors": "./src/root/selectors",
+          "#utils": "./src/root/utils",
+          "#views": "./src/root/views",
+        },
+      },
+    ],
+  ],
+};
+
