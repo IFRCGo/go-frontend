aee5cca053e4d55cc71d913aaa494627242acafe
frozenhelium <fren.ankit@gmail.com>
2020-05-18 09:09:50 +0545

    Add translate on header menu

--- src/root/lang.js
+++ src/root/lang.js
@@ -1,3 +1,8 @@
 export default {
   rcActivities: 'Red Cross / Red Crescent Activities',
+  langSelectLabel: 'Language: {currentLanguage}',
+  menuHome: 'Home',
+  menuEmergencies: 'Emergencies',
+  menuDeployments: 'Deployments',
+  menuPreparedness: 'Preparedness',
 };
