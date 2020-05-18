969fad451c45833e370304d306dfe0f1fbb678e0
frozenhelium <fren.ankit@gmail.com>
2020-05-18 09:09:50 +0545

    Add translate on header menu

--- src/root/reducers/lang.js
+++ src/root/reducers/lang.js
@@ -14,6 +14,27 @@ const initialState = {
     },
     fr: {
       rcActivities: 'Activités de la Croix-Rouge et du Croissant-Rouge',
+      langSelectLabel: 'Langue: {currentLanguage}',
+      menuHome: 'Accueil',
+      menuEmergencies: 'Urgence',
+      menuDeployments: 'Déploiements',
+      menuPreparedness: 'Préparation',
+    },
+    es: {
+      rcActivities: 'Actividades de la Cruz Roja / Media Luna Roja',
+      langSelectLabel: 'Idioma: {currentLanguage}',
+      menuHome: 'Casa',
+      menuEmergencies: 'Emergencias',
+      menuDeployments: 'Implementaciones',
+      menuPreparedness: 'Preparación',
+    },
+    ar: {
+      rcActivities: 'أنشطة الصليب الأحمر والهلال الأحمر',
+      langSelectLabel: 'لغة: {currentLanguage}',
+      menuHome: 'الصفحة الرئيسية',
+      menuEmergencies: 'الطوارئ',
+      menuDeployments: 'عمليات النشر',
+      menuPreparedness: 'التأهب',
     }
   }
 };
