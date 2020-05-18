9e0f6c2046d3965ce62118b076732ea6c937bb32
Sanjay B <sanjaybhangar@gmail.com>
2020-05-18 11:10:43 +0530

    make 2020 default selected year on overview form

--- src/root/components/per-forms/overview-form.js
+++ src/root/components/per-forms/overview-form.js
@@ -295,7 +295,7 @@ class OverviewForm extends React.Component {
                   <option value='2022'>2022</option>
                   <option value='2021'>2021</option>
 
-                  <option value='2020'>2020</option>
+                  <option value='2020' selected="selected">2020</option>
                   <option value='2019'>2019</option>
                   <option value='2018'>2018</option>
 
@@ -390,7 +390,7 @@ class OverviewForm extends React.Component {
                   <option value='2022'>2022</option>
                   <option value='2021'>2021</option>
 
-                  <option value='2020'>2020</option>
+                  <option value='2020' selected="selected">2020</option>
                   <option value='2019'>2019</option>
                   <option value='2018'>2018</option>
 
@@ -479,7 +479,7 @@ class OverviewForm extends React.Component {
                   <option value='2022'>2022</option>
                   <option value='2021'>2021</option>
 
-                  <option value='2020'>2020</option>
+                  <option value='2020' selected="selected">2020</option>
                   <option value='2019'>2019</option>
                   <option value='2018'>2018</option>
 
@@ -547,7 +547,7 @@ class OverviewForm extends React.Component {
                   <option value='2022'>2022</option>
                   <option value='2021'>2021</option>
 
-                  <option value='2020'>2020</option>
+                  <option value='2020' selected="selected">2020</option>
                   <option value='2019'>2019</option>
                   <option value='2018'>2018</option>
 
