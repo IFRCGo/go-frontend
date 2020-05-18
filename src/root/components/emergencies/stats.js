679337e5613194cad876ad853a2778fae5c45a2f
frozenhelium <fren.ankit@gmail.com>
2020-05-18 15:37:06 +0545

    Replace all relative imports with absolute imports

--- src/root/components/emergencies/stats.js
+++ src/root/components/emergencies/stats.js
@@ -3,7 +3,6 @@ import { PropTypes as T } from 'prop-types';
 
 import { environment } from '#config';
 import { commaSeparatedNumber as n } from '#utils/format';
-import { commaSeparatedNumber as n } from '../../utils/format';
 
 export default class EmergenciesStats extends React.Component {
   render () {
