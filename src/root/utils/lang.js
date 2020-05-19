0aba4ad413b8a9de44607b5ff4f29af04524b834
frozenhelium <fren.ankit@gmail.com>
2020-05-19 14:13:25 +0545

    Add translations in header

--- src/root/utils/lang.js
+++ src/root/utils/lang.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { isDefined } from '@togglecorp/fujs';
+
 
 export function resolveToString(template, params) {
   if (!isDefined(template)) {
@@ -25,3 +27,40 @@ export function resolveToString(template, params) {
   return resolvedParts.join('');
 }
 
+export function resolveToComponent(template, params) {
+  if (!isDefined(template)) {
+    return '';
+  }
+
+  const parts = template.split('{');
+  const resolvedParts = parts.map(part => {
+    const endIndex = part.indexOf('}');
+
+    if (endIndex === -1) {
+      return part;
+    }
+
+    const key = part.substring(0, endIndex);
+    if (!isDefined(params[key])) {
+      console.error(`value for key "${key}" not provided`);
+      return null;
+    }
+
+    return (
+      <React.Fragment>
+        { part.replace(`${key}}`, '') }
+        { params[key] }
+      </React.Fragment>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      {resolvedParts.map((d,i) => (
+        <React.Fragment key={i}>
+          {d}
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  );
+}
