1da6fdd3c80addf77e2a74436691350df8447042
Sameer Rai <sameerai2736@gmail.com>
2020-05-25 17:09:12 +0545

    Add translation to mobile-header and user-menu

--- src/root/components/mobile-header.js
+++ src/root/components/mobile-header.js
@@ -8,6 +8,8 @@ import { api, environment } from '#config';
 import { request } from '#utils/network';
 import { uppercaseFirstLetter as u, isoDate } from '#utils/format';
 import UserMenu from './connected/user-menu';
+import LanguageContext from '#root/languageContext';
+import Translate from '#components/Translate';
 
 function getUriForType (type, id) {
   switch (type) {
@@ -61,6 +63,7 @@ class MobileHeader extends React.PureComponent {
 
   render () {
     const { pathname } = this.props.location;
+    const { strings } = this.context;
     return (
       <header className='page__header page__header--mobile' role='banner'>
         <div className='inner'>
@@ -69,9 +72,11 @@ class MobileHeader extends React.PureComponent {
               <a href='' onClick={this.toggleMenu}></a>
             </div>
             <h1 className='page__title'>
-              <Link to='/' title='Visit page'>
+              <Link to='/' title={strings.mobileHeaderVisitHome}>
                 <img src='/assets/graphics/layout/go-logo-2020.svg' alt='IFRC GO logo'/>
-                <span>IFRC GO</span>
+                <span>
+                  <Translate stringId='mobileHeaderTitle'/>
+                </span>
               </Link>
             </h1>
           </div>
@@ -80,9 +85,11 @@ class MobileHeader extends React.PureComponent {
           <div className='nav-global-search'>
             <form className='gsearch'>
               <div>
-                <label className='form__label'>Search</label>
+                <label className='form__label'>
+                  <Translate stringId='mobileHeaderSearchTitle'/>
+                </label>
                 <Select.Async
-                  placeholder='Search...'
+                  placeholder={strings.mobileHeaderSearch}
                   onChange={this.onSelect}
                   loadOptions={this.getOptions} />
               </div>
@@ -94,29 +101,33 @@ class MobileHeader extends React.PureComponent {
         })}>
           <div className='inner'>
             <div className='mobile__actions'>
-              <Link to='/reports/new' className='button button--small button--primary-filled' title='Create Field Report'><span>Create Field Report</span></Link>
+              <Link to='/reports/new' className='button button--small button--primary-filled' title={strings.mobileHeaderCreateFieldReport}><span><Translate stringId='mobileHeaderCreateFieldReport'/></span></Link>
               <button className='button button--small button--close button--text-hidden' onClick={this.toggleMenu}>
-                <span>close</span>
+                <span>
+                  <Translate stringId='mobileHeaderClose'/>
+                </span>
               </button>
             </div>
             <ul className='nav-block-menu'>
-              <li><Link to='/' title='Visit Home page' className={c({'disabled': pathname === '/'})}><span>Home</span></Link></li>
-              <li><Link to='/emergencies' title='Visit emergencies page' className={c({'disabled': pathname === '/emergencies'})}><span>Emergencies</span></Link></li>
+              <li><Link to='/' title={strings.mobileHeaderVisitHome} className={c({'disabled': pathname === '/'})}><span><Translate stringId='mobileHeaderHome'/></span></Link></li>
+              <li><Link to='/emergencies' title={strings.mobileHeaderVisitEmergencies} className={c({'disabled': pathname === '/emergencies'})}><span><Translate stringId='mobileHeaderEmergencies'/></span></Link></li>
               <li>
-                <span>Regions</span>
+                <span>
+                  <Translate stringId='mobileHeaderRegion'/>
+                </span>
                 <ul className='nav-block-submenu'>
-                  <li><Link to='/regions/0' title='Visit Africa region page' className={c({'disabled': pathname === 'regions/0'})}><span>Africa</span></Link></li>
-                  <li><Link to='/regions/1' title='Visit Americas region page' className={c({'disabled': pathname === 'regions/1'})}><span>Americas</span></Link></li>
-                  <li><Link to='/regions/2' title='Visit Asia Pacific region page' className={c({'disabled': pathname === 'regions/2'})}><span>Asia Pacific</span></Link></li>
-                  <li><Link to='/regions/3' title='Visit Europe region page' className={c({'disabled': pathname === 'regions/3'})}><span>Europe</span></Link></li>
-                  <li><Link to='/regions/4' title='Visit Middle East & North Africa region page' className={c({'disabled': pathname === 'regions/0'})}><span>Middle East & North Africa</span></Link></li>
+                  <li><Link to='/regions/0' title={strings.mobileHeaderVisitAfrica} className={c({'disabled': pathname === 'regions/0'})}><span><Translate stringId='mobileHeaderAfrica'/></span></Link></li>
+                  <li><Link to='/regions/1' title={strings.mobileHeaderVisitAmerica} className={c({'disabled': pathname === 'regions/1'})}><span><Translate stringId='mobileHeaderAmerica'/></span></Link></li>
+                  <li><Link to='/regions/2' title={strings.mobileHeaderVisitAsia} className={c({'disabled': pathname === 'regions/2'})}><span><Translate stringId='mobileHeaderAsia'/></span></Link></li>
+                  <li><Link to='/regions/3' title={strings.mobileHeaderVisitEurope} className={c({'disabled': pathname === 'regions/3'})}><span><Translate stringId='mobileHeaderEurope'/></span></Link></li>
+                  <li><Link to='/regions/4' title={strings.mobileHeaderVisitMiddleEast} className={c({'disabled': pathname === 'regions/0'})}><span><Translate stringId='mobileHeaderMiddleEast'/></span></Link></li>
                 </ul>
               </li>
-              <li><Link to='/deployments' title='Visit Deployments page' className={c({'disabled': pathname === '/deployments'})}><span>Deployments</span></Link></li>
-              <li><Link to='/about' title='Visit Resources page' className={c({'disabled': pathname === '/about'})}><span>Resources</span></Link></li>
+              <li><Link to='/deployments' title={strings.mobileHeaderVisitDeployments} className={c({'disabled': pathname === '/deployments'})}><span><Translate stringId='mobileHeaderDeployments'/></span></Link></li>
+              <li><Link to='/about' title={strings.mobileHeaderVisitResources} className={c({'disabled': pathname === '/about'})}><span><Translate stringId='mobileHeaderResources'/></span></Link></li>
             </ul>
             <ul className='nav-block-user-menu' role='menu'>
-              <li><Link to='/account' title='View user account' className={c('user__menu-item', {'disabled': pathname === '/account'})}>Account</Link></li>
+              <li><Link to='/account' title={strings.mobileHeaderVisitAccount} className={c('user__menu-item', {'disabled': pathname === '/account'})}><Translate stringId='mobileHeaderAccount'/></Link></li>
               <UserMenu />
             </ul>
           </div>
@@ -133,4 +144,5 @@ if (environment !== 'production') {
   };
 }
 
+MobileHeader.contextType = LanguageContext;
 export default withRouter(MobileHeader);
