f1e7e66022eeaa66078ae1a73d67bd0fe61c2225
Sameer Rai <sameerai2736@gmail.com>
2020-05-29 15:25:49 +0545

    Add translation to video-carousel

--- src/root/lang.js
+++ src/root/lang.js
@@ -581,6 +581,7 @@ export default {
   fieldReportAffectedPopOther: 'Affected Pop Centres (Other): ',
   fieldReportNumericTitle: 'Numeric details',
   fieldReportSourcesOther: 'Sources for data marked as Other',
+
   deploymentsTitle: 'IFRC Go - Deployments',
   deploymentsDeployedERU: 'Deployed ERU\'s',
   deploymentsDeployedRR: 'Deployed Rapid Response',
@@ -624,4 +625,15 @@ export default {
   emergencyMapBorderDisputed: 'Disputed borders',
   emergencyMapTerritory: 'territory',
   emergencyMapDisputedTerritory: 'Disputed territory',
+
+  videoCarouselHeading1: '3W - Who does What, Where.',
+  videoCarouselSubHeading1: 'The "Who does What, Where", or 3W, is a GO platform tool to enhance coordination, improve disaster analysis, and elevate the speed and quality of Red Cross Red Crescent emergency response.',
+  videoCarouselHeading2: '3W Instructional',
+  videoCarouselSubHeading2: 'View this video tutorial on how to manage the 3W tool data  (Who, What, Where) and generate useful and visually pleasant information products on the GO Platform - International Federation of Red Cross Red Crescent (IFRC).',
+  videoCarouselHeading3: 'Field Report COVID-19',
+  videoCarouselSubHeading3: 'View this video tutorial on how to create a "Field Report" COVID-19 on the GO Platform - International Federation of Red Cross Red Crescent (IFRC).',
+  videoCarouselHeading4: 'Preparedness for Effective Response Module',
+  videoCarouselSubHeading4: 'View this video tutorial on how to navigate on the Preparedness for Effective Response (PER) module on the GO Platform - International Federation of Red Cross Red Crescent (IFRC).',
+  videoCarouselHeading5: 'Subscriptions to User Notifications',
+  videoCarouselSubHeading5: 'View this video tutorial on how to "Subscribe to User Notifications" on the GO Platform - International Federation of Red Cross Red Crescent (IFRC).'
 };
