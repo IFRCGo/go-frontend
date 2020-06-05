cb120adf493b7b2df3088c7f9252811dab0c0bc7
Sameer Rai <sameerai2736@gmail.com>
2020-06-05 16:10:24 +0545

    Add translation to CountryOverview

--- src/root/views/CountryOverview/PastOperation.js
+++ src/root/views/CountryOverview/PastOperation.js
@@ -13,6 +13,8 @@ import {
 } from 'recharts';
 
 import { countryNameMapById } from '#utils/field-report-constants';
+import Translate from '#components/Translate';
+
 
 const DetailElement = ({
   value,
@@ -130,24 +132,25 @@ class PastOperations extends React.PureComponent {
     const { chartMode } = this.state;
     const EventDetailTooltip = this.renderEventDetailTooltip;
     const chartData = this.getChartData(data);
+    const { strings } = this.context;
 
     return (
       <div className={_cs(className, 'overview-past-operations')}>
         <h3 className='tc-heading'>
-          Past operations
+          <Translate stringId='pastOperationsTitle'/>
         </h3>
         <div className='past-operations-filter'>
           <div className='chart-mode-input'>
             <SegmentButton
               onClick={this.handleChartModeChange}
-              label='People targeted'
+              label={strings.pastOperationsPeopleTargeted}
               segmentKey='num_beneficiaries'
               className='chart-mode-button'
               isActive={chartMode === 'num_beneficiaries'}
             />
             <SegmentButton
               onClick={this.handleChartModeChange}
-              label="Funding requirements"
+              label={strings.pastOperationsFunding}
               className='chart-mode-button'
               segmentKey='amount_requested'
               isActive={chartMode === 'amount_requested'}
