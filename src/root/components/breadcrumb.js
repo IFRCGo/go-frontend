2ae72ba0da49946fcc2a5a879807267aed4586a8
imohkay <karen.menezes@gmail.com>
2020-06-16 13:25:16 +0530

    #770: Add wrapper container for breadcrumb block to align with top header

--- src/root/components/breadcrumb.js
+++ src/root/components/breadcrumb.js
@@ -5,16 +5,18 @@ import { NavLink } from 'react-router-dom';
 
 export default function BreadCrumb ({crumbs}) {
   return (
-    <div className='breadcrumb__block'>
-      {crumbs.map((crumb, i) => (
-        <BreadcrumbsItem order={i} key={crumb.name} to={crumb.link} className='breadcrumb'>{crumb.name}</BreadcrumbsItem>
-      ))}
-      <Breadcrumbs
-        separator={<span className="breadcrumb__next"> > </span>}
-        item={NavLink}
-        compare={(a,b) => b.order - a.order}
-        finalItem={'b'}
-      />
+    <div className='row row--centered'>
+      <div className='breadcrumb__block'>
+        {crumbs.map((crumb, i) => (
+          <BreadcrumbsItem order={i} key={crumb.name} to={crumb.link} className='breadcrumb'>{crumb.name}</BreadcrumbsItem>
+        ))}
+        <Breadcrumbs
+          separator={<span className="breadcrumb__next"> > </span>}
+          item={NavLink}
+          compare={(a,b) => b.order - a.order}
+          finalItem={'b'}
+        />
+      </div>
     </div>
   );
 }
