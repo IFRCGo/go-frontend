06db11ef7d15c5529e49dbe533382d167ad0ab0f
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-07-23 10:23:48 +0200

    Turned why-did-you-render off by default

--- src/wdyr.js
+++ src/wdyr.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && false) {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
