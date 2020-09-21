3e8f2d434fd24ccde0c9fcee570c9718e3ceb02d
Sajjad Anwar <sajjadkm@gmail.com>
2020-09-21 17:39:10 +0530

    add missing utils

---
+++ src/root/utils/country-labels.js
@@ -0,0 +1,25 @@
+export const countryLabels = {
+  'id': 'countryLabels',
+  'source': 'countryCentroids',
+  'type': 'symbol',
+  layout: {
+    'text-field': ['get', 'name'],
+    'text-font': ['Arial Unicode MS Regular'],
+    'text-letter-spacing': 0.15,
+    'text-line-height': 1.2,
+    'text-max-width': 8,
+    'text-justify': 'center',
+    'text-anchor': 'top',
+    'text-padding': 2,
+    'text-size': [
+      'interpolate', ['linear', 1], ['zoom'],
+      0, 6,
+      6, 16
+    ]
+  },
+  paint: {
+   'text-color': '#2d4662',
+   'text-halo-color': '#2d4662',
+   'text-halo-width': 0.2
+  }
+};
