780c29cbb0a0451cbe46a5ef7c3e6d12694bb0ed
Horváth Gergely <gergelyzsolt.hrv@gmail.com>
2020-09-25 17:36:15 +0200

    Checkie-checkie

---
+++ public/checkie.js
@@ -0,0 +1,7 @@
+var message = "\
+  <h1 style='text-align:center;font-size:2rem;color:#f5333f;font-family:Poppins,\'Helvetica Neue\',Helvetica,Arial,sans-serif;'>\
+    Internet Explorer is not supported by GO. Please consider using Google Chrome, Mozilla Firefox, Edge (from version 79), or an other Chromium based browser.\
+  </h1>\
+";
+document.body.style.backgroundColor = "#faf9f9";
+document.getElementById("app-container").innerHTML = message;
