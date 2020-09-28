05ec6827cd50f3bac3bf71ee6c9e73efa08270d1
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-09-28 15:34:54 +0200

    Added IE check

--- public/checkie.js
+++ public/checkie.js
@@ -1,7 +1,14 @@
-var message = "\
-  <h1 style='text-align:center;font-size:2rem;color:#f5333f;font-family:Poppins,\'Helvetica Neue\',Helvetica,Arial,sans-serif;'>\
-    Internet Explorer is not supported by GO. Please consider using Google Chrome, Mozilla Firefox, Edge (from version 79), or an other Chromium based browser.\
-  </h1>\
-";
-document.body.style.backgroundColor = "#faf9f9";
-document.getElementById("app-container").innerHTML = message;
+// https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+// Duck typing method, not fully failproof
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isIE === true) {
+  var message = "\
+    <h1 style='text-align:center;font-size:2rem;color:#f5333f;font-family:Poppins,\'Helvetica Neue\',Helvetica,Arial,sans-serif;'>\
+      Internet Explorer is not supported by GO. Please consider using Google Chrome, Mozilla Firefox, Edge (from version 79), or an other Chromium based browser.\
+    </h1>\
+  ";
+  document.body.style.backgroundColor = "#faf9f9";
+  document.getElementById("app-container").innerHTML = message;
+}
