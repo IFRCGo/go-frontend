b5cc92a133df71c985c8584f4c431d4014d70b25
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2020-11-19 09:47:10 +0100

    Comment out import

--- src/root/views/app.js
+++ src/root/views/app.js
@@ -6,7 +6,7 @@ import Header from '#components/header';
 import MobileHeader from '#components/mobile-header';
 import Footer from '#components/footer';
 import GlobalLoading from '#components/global-loading';
-import GlobalHeaderBanner from '#components/global-header-banner';
+// import GlobalHeaderBanner from '#components/global-header-banner';
 import SysAlerts from '#components/system-alerts';
 
 import {
