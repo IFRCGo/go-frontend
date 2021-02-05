bdbb8658fb434483d7001574f0446023b4523983
GregoryHorvath <gergelyzsolt.hrv@gmail.com>
2021-02-05 15:17:06 +0100

    Handling null title for tooltips

--- src/root/components/common/tooltip.js
+++ src/root/components/common/tooltip.js
@@ -7,19 +7,24 @@ import ReactTooltip from 'react-tooltip';
 const Tooltip = ({title, description}) => {
   return (
     <React.Fragment>
-      <a data-tip data-for={title} data-event='click focus'>
+      <a data-tip data-for={title ? title : description} data-event='click focus'>
         <InfoIcon className='tooltip' />
       </a>
       <ReactTooltip
         className='tooltip'
         globalEventOff='click'
-        id={title}
+        id={title ? title : description}
         aria-haspopup='true'
       >
-        <header className='tooltip__header' >
-          {title}
-        </header>
-        <span className='tooltip__text' dangerouslySetInnerHTML={{ __html: description }} ></span>
+        { title
+          ? (
+            <header className='tooltip__header' >
+              {title}
+            </header>
+          ) : null }
+        { description
+          ? (<span className='tooltip__text' dangerouslySetInnerHTML={{ __html: description }} ></span>)
+          : null }
       </ReactTooltip>
     </React.Fragment>
   );
@@ -40,7 +45,7 @@ export const HoverTooltip = ({title, description, id}) => {
         : null }
       { description
         ? (<span className='tooltip__text' dangerouslySetInnerHTML={{ __html: description }} ></span>)
-        : null }      
+        : null }
     </ReactTooltip>
   );
 };
