28ee097cbfe4ea1b11926dfd9b92ddb569e65313
barshathakuri <barshathakuri708@gmail.com>
2021-06-10 18:30:00 +0545

    Add display data

--- src/root/views/GlobalThreeW/index.tsx
+++ src/root/views/GlobalThreeW/index.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import Page from '#components/Page';
-import Heading, { Props as HeadingProps } from '#components/Heading';
-import Description from '#components/Description';
 import { _cs } from '@togglecorp/fujs';
 import LanguageContext from '#root/languageContext';
+import Container from '#components/Container';
+import BreadCrumb from '#components/breadcrumb';
+import {
+  useRequest
+} from '#utils/restRequest';
 
 import styles from './styles.module.scss';
-import Container from '#components/Container';
-import PageHeader from '#components/PageHeader';
-import ThreeWForm from '#components/ThreeWForm';
-import BreadCrumb from '#components/breadcrumb';
+
 
 interface Props {
   className?: string;
   location: Location;
+}
+
+interface ProjectOverview {
+  target_total?: number | string;
+  active_societies?: number | string;
+  on_going_projects?: number | string;
+}
+
+interface NsProjectsOverviewFields {
+  ongoing_projects: number;
+  target_total: number;
+  society_name: string;
+  name: string;
+}
+
+interface NsProjectOverview {
+  results: NsProjectsOverviewFields[];
+}
+
+interface GlobalProjectsOverviewFields {
+  total_ongoing_projects: number;
+  ns_with_ongoing_activities: number;
+  target_total: number;
 }
 
 function GlobalThreeW(props: Props) {
@@ -21,15 +44,46 @@ function GlobalThreeW(props: Props) {
     className,
     location
   } = props;
+
+  const {
+    pending: nsProjectsPending,
+    response: nsProjectsResponse,
+  } = useRequest<NsProjectOverview>({
+    url: `api/v2/global-project/ns-ongoing-projects-stats/`,
+  });
+
+  const {
+    pending: projectsOverviewPending,
+    response: projectsOverviewResponse,
+  } = useRequest<GlobalProjectsOverviewFields>({
+    url: `api/v2/global-project/overview/`,
+  });
+
+  const pending = projectsOverviewPending || nsProjectsPending;
+
   const { strings } = React.useContext(LanguageContext);
 
-  const crumbs = React.useMemo(() => [
-    // FIXME: use translations
-    { link: location?.pathname, name: 'GlobalThreeW' },
+  const [projectOverview, setProjectOverview] = React.useState<ProjectOverview>({ target_total: 'n/a', active_societies: 'n/a', on_going_projects: 'n/a' });
 
+  const crumbs = React.useMemo(() => [
+    { link: location?.pathname, name: 'GlobalThreeW' },
     { link: '/', name: strings.breadCrumbHome },
   ], [strings.breadCrumbHome, strings.breadCrumbNewFieldReport, location]);
 
+  React.useEffect(() => {
+    if (nsProjectsResponse && projectsOverviewResponse) {
+      const totalprojects = nsProjectsResponse.results
+        .map((r: NsProjectsOverviewFields) => r.ongoing_projects)
+        .reduce((a, b) => a + b, 0);
+
+      const overview: ProjectOverview = {
+        target_total: projectsOverviewResponse.target_total,
+        active_societies: projectsOverviewResponse.ns_with_ongoing_activities,
+        on_going_projects: totalprojects,
+      };
+      setProjectOverview(overview);
+    }
+  }, [nsProjectsResponse, projectsOverviewResponse,]);
 
   return (
     <Page
@@ -38,8 +92,16 @@ function GlobalThreeW(props: Props) {
       breadCrumbs={<BreadCrumb crumbs={crumbs} compact />}
     >
       <Container>
-        Hello World
-        </Container>
+        <div>
+          {projectOverview.on_going_projects}
+        </div>
+        <div>
+          {projectOverview.active_societies}
+        </div>
+        <div>
+          {projectOverview.target_total}
+        </div>
+      </Container>
     </Page>
   );
 }
