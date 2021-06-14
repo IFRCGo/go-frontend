faf33f426145f4b5935ba154a6113e0277729c83
frozenhelium <fren.ankit@gmail.com>
2021-06-14 09:56:43 +0545

    Improve the global 3w logic

--- src/root/views/GlobalThreeW/index.tsx
+++ src/root/views/GlobalThreeW/index.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import Page from '#components/Page';
 import { _cs } from '@togglecorp/fujs';
-import LanguageContext from '#root/languageContext';
+
+import BlockLoading from '#components/block-loading';
 import Container from '#components/Container';
 import BreadCrumb from '#components/breadcrumb';
-import {
-  useRequest
-} from '#utils/restRequest';
+import LanguageContext from '#root/languageContext';
+import { useRequest } from '#utils/restRequest';
 
 import styles from './styles.module.scss';
-
-
-interface Props {
-  className?: string;
-  location: Location;
-}
-
-interface ProjectOverview {
-  target_total?: number | string;
-  active_societies?: number | string;
-  on_going_projects?: number | string;
-}
 
 interface NsProjectsOverviewFields {
   ongoing_projects: number;
@@ -37,6 +25,12 @@ interface GlobalProjectsOverviewFields {
   total_ongoing_projects: number;
   ns_with_ongoing_activities: number;
   target_total: number;
+}
+
+
+interface Props {
+  className?: string;
+  location: Location;
 }
 
 function GlobalThreeW(props: Props) {
@@ -63,43 +57,39 @@ function GlobalThreeW(props: Props) {
 
   const { strings } = React.useContext(LanguageContext);
 
-  const [projectOverview, setProjectOverview] = React.useState<ProjectOverview>({ target_total: 'n/a', active_societies: 'n/a', on_going_projects: 'n/a' });
-
   const crumbs = React.useMemo(() => [
     { link: location?.pathname, name: 'GlobalThreeW' },
     { link: '/', name: strings.breadCrumbHome },
-  ], [strings.breadCrumbHome, strings.breadCrumbNewFieldReport, location]);
+  ], [strings.breadCrumbHome, location]);
 
-  React.useEffect(() => {
-    if (nsProjectsResponse && projectsOverviewResponse) {
-      const totalprojects = nsProjectsResponse.results
-        .map((r: NsProjectsOverviewFields) => r.ongoing_projects)
-        .reduce((a, b) => a + b, 0);
+  const numTotalProjects = React.useMemo(() => {
+    const totalProjects = nsProjectsResponse?.results
+    .map((r: NsProjectsOverviewFields) => r.ongoing_projects)
+    .reduce((acc, val) => acc + val, 0);
 
-      const overview: ProjectOverview = {
-        target_total: projectsOverviewResponse.target_total,
-        active_societies: projectsOverviewResponse.ns_with_ongoing_activities,
-        on_going_projects: totalprojects,
-      };
-      setProjectOverview(overview);
-    }
-  }, [nsProjectsResponse, projectsOverviewResponse,]);
+    return totalProjects;
+  }, [nsProjectsResponse]);
+
+  const numActiveSocieties = projectsOverviewResponse?.ns_with_ongoing_activities;
+  const numOngoingProjects = projectsOverviewResponse?.target_total;
 
   return (
     <Page
+      className={_cs(styles.globalThreeW, className)}
       title="IFRC Go -Global 3W Response"
       heading="Global 3W Response"
       breadCrumbs={<BreadCrumb crumbs={crumbs} compact />}
     >
+      {pending && <BlockLoading /> }
       <Container>
         <div>
-          {projectOverview.on_going_projects}
+          {numOngoingProjects}
         </div>
         <div>
-          {projectOverview.active_societies}
+          {numActiveSocieties}
         </div>
         <div>
-          {projectOverview.target_total}
+          {numTotalProjects}
         </div>
       </Container>
     </Page>
