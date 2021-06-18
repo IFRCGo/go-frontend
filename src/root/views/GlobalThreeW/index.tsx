7cfee0f0f35df13cd45e45221f4fc8300512619b
frozenhelium <fren.ankit@gmail.com>
2021-06-18 16:32:39 +0545

    Improve global 3w charts styling

--- src/root/views/GlobalThreeW/index.tsx
+++ src/root/views/GlobalThreeW/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import Page from '#components/Page';
 import { _cs } from '@togglecorp/fujs';
+import { IoAdd } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 
 import BlockLoading from '#components/block-loading';
+import Page from '#components/Page';
+import { useButtonFeatures } from '#components/Button';
 import Card from '#components/Card';
 import KeyFigure from '#components/KeyFigure';
 import Container from '#components/Container';
 import BreadCrumb from '#components/breadcrumb';
 import ExportProjectsButton from '#components/ExportProjectsButton';
 import LanguageContext from '#root/languageContext';
-import { useRequest } from '#utils/restRequest';
+import { useRequest, ListResponse } from '#utils/restRequest';
 import { sum } from '#utils/common';
 
-import { GoBarChart, GoPieChart } from './Charts';
+import {
+  ThreeWBarChart,
+  ThreeWPieChart,
+} from './Charts';
 
 import styles from './styles.module.scss';
 
@@ -23,23 +29,31 @@ interface NsProjectsOverviewFields {
   name: string;
 }
 
-interface NsProjectOverview {
-  results: NsProjectsOverviewFields[];
-}
-
-interface ProgrammePerSector {
+interface ProjectPerProgrammeType {
   programme_type: number;
   programme_type_display: string;
   count: number;
+}
+
+interface ProjectPerSector {
+  count: number;
+  primary_sector: number;
+  primary_sector_display: string;
+}
+
+interface ProjectPerSecondarySector {
+  count: number;
+  secondary_sector: number;
+  secondary_sector_display: string;
 }
 
 interface GlobalProjectsOverviewFields {
   total_ongoing_projects: number;
   ns_with_ongoing_activities: number;
   target_total: number;
-  projects_per_sector: any[];
-  projects_per_programme_type: ProgrammePerSector[];
-  projects_per_secondary_sectors: any[];
+  projects_per_sector: ProjectPerSector[];
+  projects_per_programme_type: ProjectPerProgrammeType[];
+  projects_per_secondary_sectors: ProjectPerSecondarySector[];
 }
 
 interface Props {
@@ -56,7 +70,7 @@ function GlobalThreeW(props: Props) {
   const {
     pending: nsProjectsPending,
     response: nsProjectsResponse,
-  } = useRequest<NsProjectOverview>({
+  } = useRequest<ListResponse<NsProjectsOverviewFields>>({
     url: 'api/v2/global-project/ns-ongoing-projects-stats/',
   });
 
@@ -83,11 +97,45 @@ function GlobalThreeW(props: Props) {
   const numActiveSocieties = projectsOverviewResponse?.ns_with_ongoing_activities;
   const numOngoingProjects = projectsOverviewResponse?.target_total;
 
+  const linkProps = useButtonFeatures({
+    variant: "secondary",
+    icons: <IoAdd />,
+    children: "Add 3W Project",
+  });
+
+  const projectPerSectorChartData = projectsOverviewResponse?.projects_per_sector.map((p) => ({
+    key: p.primary_sector,
+    value: p.count,
+    name: p.primary_sector_display,
+  })) ?? [];
+
+  const projectPerSecondarySectorChartData = projectsOverviewResponse?.projects_per_secondary_sectors
+    .map((p) => ({
+      key: p.secondary_sector,
+      value: p.count,
+      name: p.secondary_sector_display,
+    })) ?? [];
+
+  const projectPerProgrammeTypeChartData = projectsOverviewResponse?.projects_per_programme_type
+    .map((p) => ({
+      key: p.programme_type,
+      value: p.count,
+      name: p.programme_type_display,
+    })) ?? [];
+
+  console.info(projectsOverviewResponse?.projects_per_secondary_sectors);
+
   return (
     <Page
       className={_cs(styles.globalThreeW, className)}
       title="IFRC Go - Global 3W Response"
       heading="Global 3W Response"
+      actions={(
+        <Link
+          to="/three-w/new"
+          {...linkProps}
+        />
+      )}
       description="Description lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ligula sem, tempus et iaculis quis, auctor ut elit. Ut vitae eros quis nunc fringilla ultrices."
       breadCrumbs={<BreadCrumb crumbs={crumbs} compact />}
       infoContainerClassName={styles.infoContainer}
@@ -119,27 +167,34 @@ function GlobalThreeW(props: Props) {
           </Card>
         </>
       )}
-      actions={(
-        <ExportProjectsButton
-          fileNameSuffix="All projects"
-        />
-      )}
     >
       {pending ? <BlockLoading /> : (
-        <Container>
-          {projectsOverviewResponse && <GoPieChart
-            heading="Programme Type"
-            data={projectsOverviewResponse.projects_per_programme_type} />}
-
-          {projectsOverviewResponse && <GoBarChart
-            heading="Project Per Sector"
-            data={projectsOverviewResponse.projects_per_sector} />}
-
-          {projectsOverviewResponse && <GoBarChart
-            heading="Top Tags"
-            data={projectsOverviewResponse.projects_per_secondary_sectors}
-          />}
-        </Container>
+        <>
+          <Container>
+            <ExportProjectsButton
+              fileNameSuffix="All projects"
+            />
+          </Container>
+          <Container
+            contentClassName={styles.chartsContainer}
+          >
+            <ThreeWBarChart
+              className={styles.projectPerSectorChart}
+              heading="Project Per Sector"
+              data={projectPerSectorChartData}
+            />
+            <ThreeWPieChart
+              className={styles.programmeTypeChart}
+              heading="Programme Type"
+              data={projectPerProgrammeTypeChartData}
+            />
+            <ThreeWBarChart
+              className={styles.topTagsChart}
+              heading="Top Tags"
+              data={projectPerSecondarySectorChartData}
+            />
+          </Container>
+        </>
       )}
     </Page>
   );
