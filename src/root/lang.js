edb13623a6c88c25b714bd619b38c71d9238da3c
frozenhelium <fren.ankit@gmail.com>
2021-08-13 11:39:26 +0545

    Improve my dref applications view

--- src/root/lang.js
+++ src/root/lang.js
@@ -2994,7 +2994,7 @@ export default {
   drefTableAppealNumber: 'Appeal Number',
   drefTableName: 'Name',
   drefTableRegionalTechReview: 'Regional Technical Review',
-  drefTableSubToGeneva: 'Submitted to Geneva',
+  drefTableSubmittedToGeneva: 'Submitted to Geneva',
   drefTableLastUpdate: 'Last Update',
   drefInProgressApplications: 'In progress Applications',
   drefApprovedApplications: 'Approved Applications'
