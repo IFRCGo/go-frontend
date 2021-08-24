beccfbbcb6d94233d4e020755b323f71f6f45cd9
frozenhelium <fren.ankit@gmail.com>
2021-08-24 15:38:19 +0545

    Add remove features in array fields

--- src/root/lang.js
+++ src/root/lang.js
@@ -1650,6 +1650,7 @@ export default {
   plannedIntervention: 'Planned Intervention',
   interventionsLabel: 'Select the interventions that apply.',
   nationalSocietyStrengthening: 'National Society strengthening',
+  secretariatServices: 'Secretariat services',
 
   //Sumission
   submissionFlow: 'Submission flow',
