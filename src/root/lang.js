1bb4f05291c14166a2117d48e3d7a7e7bf8319ea
Sameer Rai <sameerai2736@gmail.com>
2021-08-31 13:55:27 +0545

    Use file upload api to save files and update dref form

--- src/root/lang.js
+++ src/root/lang.js
@@ -2960,7 +2960,7 @@ export default {
   timeFrames: 'Timeframes',
   eventDate: 'Date of event',
   goFieldReportDate: 'Go Field Report Date',
-  triggerDate: 'Disaster date/ trigger date',
+  triggerDate: 'Disaster date / trigger date',
   nsResponseStarted: 'NS response commenced',
   uploadMap: 'Upload map',
 
