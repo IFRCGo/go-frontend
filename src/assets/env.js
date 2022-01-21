0f206d91a0b42727f8ac94a61d7cb3b6e5c9519f
Viktor Deak <viktor.deak@atos.net>
2022-01-21 12:58:17 +0100

    assets/env.js added

---
+++ src/assets/env.js
@@ -0,0 +1,5 @@
+window.environment = 'development'
+window.api = 'https://dsgocdnapi.azureedge.net/'
+window.adminUrl = 'https://dscgocdnapi.azureedge.net/'
+window.fdrsAuth = ''
+window.mbtoken = 'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg'
