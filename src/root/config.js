264a0bceae3738272e1432aacf34422755a1af4a
Viktor Deak <viktor.deak@atos.net>
2022-02-23 16:59:35 +0100

    fix - config.js

--- src/root/config.js
+++ src/root/config.js
@@ -23,13 +23,24 @@ const defaultUrls = {
 const defaultMapboxToken =  'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg';
 
 
-export const riskApi = window.riskApi;
-export const riskAdminUrl = window.riskAdminUrl;
-export const environment = window.environmentDisplayName;
-export const api = window.api;
-export const adminUrl = window.adminUrl;
-export const fdrsAuth = window.fdrsAuth;
-export const mbtoken = window.mbtoken;
+//export const riskApi = window.riskApi;
+//export const riskAdminUrl = window.riskAdminUrl;
+//export const environment = window.environmentDisplayName;
+//export const api = window.api;
+//export const adminUrl = window.adminUrl;
+//export const fdrsAuth = window.fdrsAuth;
+//export const mbtoken = window.mbtoken;
+//export const showEnvBanner = window.showEnvBanner;
+
+export const environment = process.env.REACT_APP_ENVIRONMENT || process.env.NODE_ENV || 'development';
+
+export const api = process.env.REACT_APP_API_ENDPOINT ?? defaultUrl?.apiServer;
+export const adminUrl = process.env.REACT_APP_ADMIN_URL ?? defaultUrl?.adminPanel;
+export const riskApi = process.env.REACT_APP_RISK_API_ENDPOINT ?? defaultUrl?.riskApiServer;
+export const riskAdminUrl = process.env.REACT_APP_RISK_ADMIN_URL ?? defaultUrl?.riskAdminUrl;
+export const fdrsAuth = process.env.REACT_APP_FDRS_AUTH ?? process.env['fdrsAuth'] ?? '';
+export const mbtoken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN ?? defaultMapboxToken;
+
 export const showEnvBanner = window.showEnvBanner;
 
 const defaultUrl = defaultUrls[environment];
