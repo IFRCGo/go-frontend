0383ddc533e48f86076b23ecef04271ef1ac4a16
Szabo, Zoltan <szabozoltan69@gmail.com>
2022-04-14 16:26:23 +0200

    Fix api url

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,8 +1,8 @@
 window.appSettings = {};
 
-window.appSettings.environmentDisplayName = 'Staging site';
-window.appSettings.apiUrl = 'https://dsgocdnapi.azureedge.net/';
-window.appSettings.adminUrl = 'https://dsgocdnapi.azureedge.net/';
+window.appSettings.environmentDisplayName = 'Prod site';
+window.appSettings.apiUrl = 'https://goadmin.ifrc.org/';
+window.appSettings.adminUrl = 'https://goadmin.ifrc.org/';
 window.appSettings.fdrsAuth = '';
 window.appSettings.mbtoken = 'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg';
 window.appSettings.showEnvBanner = 'false';
