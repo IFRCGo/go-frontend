9b1353c7b0feed0286abedb2a7697d7047421bb1
frozenhelium <ankit.mehta@togglecorp.com>
2022-04-25 16:43:32 +0545

    Add fallback env config

--- src/root/config.js
+++ src/root/config.js
@@ -1,11 +1,26 @@
-export const riskApi = window.appSettings.riskApi;
-export const riskAdminUrl = window.appSettings.riskAdminUrl;
-export const environment = window.appSettings.environmentDisplayName;
-export const api = window.appSettings.apiUrl;
-export const adminUrl = window.appSettings.adminUrl;
-export const fdrsAuth = window.appSettings.fdrsAuth;
-export const mbtoken = window.appSettings.mbtoken;
-export const showEnvBanner = window.appSettings.showEnvBanner;
+// NOTE: for some reason, old pipeline doesn't load env.js file and window.appSettings is undefined
+if (!window.appSettings) {
+  window.appSettings = {
+    environmentDisplayName: 'Staging site',
+    apiUrl: 'https://dsgocdnapi.azureedge.net/',
+    adminUrl: 'https://dsgocdnapi.azureedge.net/',
+    fdrsAuth: '',
+    mbtoken: 'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg',
+    showEnvBanner: 'false',
+    riskApi: 'https://go-risk-staging.northeurope.cloudapp.azure.com/',
+    riskAdminUrl: 'https://go-risk-staging.northeurope.cloudapp.azure.com/',
+  };
+}
+
+export const environment = process.env.REACT_APP_ENVIRONMENT ?? 'development';
+
+export const riskApi = process.env.REACT_APP_RISK_API_ENDPOINT ?? window.appSettings.riskApi;
+export const riskAdminUrl = process.env.REACT_APP_RISK_ADMIN_URL ?? window.appSettings.riskAdminUrl;
+export const api = process.env.REACT_APP_API_ENDPOINT ?? window.appSettings.apiUrl;
+export const adminUrl = process.env.REACT_APP_ADMIN_URL ?? window.appSettings.adminUrl;
+export const fdrsAuth = process.env.REACT_APP_FDRS_AUTH ?? window.appSettings.fdrsAuth;
+export const mbtoken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN ?? window.appSettings.mbtoken;
+export const showEnvBanner = process.env.REACT_APP_SHOW_ENV_BANNER ?? window.appSettings.showEnvBanner;
 
 
 const config = {
