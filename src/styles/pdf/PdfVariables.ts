54f492c9a172ee81e174450988b112f78d7f0271
puranban <puranban77@gmail.com>
2022-07-13 13:58:11 +0545

    split resuable code of export

---
+++ src/styles/pdf/PdfVariables.ts
@@ -0,0 +1,28 @@
+export const PAGE_PADDING = 20;
+export const FULL_WIDTH = 600;
+export const VERTICAL_MARGIN = 10;
+export const TABLE_CELL_PADDING = 10;
+export const HEADING_VERTICAL_PADDING = 10;
+export const SUBHEADING_VERTICAL_PADDING = 5;
+export const SMALL_PADDING = 7;
+export const TABLE_BORDER = '1px solid #ffffff';
+
+export const FONT_SIZE_MEDIUM = 10;
+export const FONT_SIZE_LARGE = 12;
+export const FONT_SIZE_EXTRA_LARGE = 15;
+export const FONT_SIZE_SUPER_LARGE = 20;
+export const FONT_SIZE_MEGA_LARGE = 27;
+export const FONT_SIZE_ULTRA_LARGE = 40;
+
+export const SPACING_SMALL = 6;
+export const SPACING_MEDIUM = 10;
+export const SPACING_LARGE = 16;
+export const SPACING_SUPER_LARGE = 32;
+export const SECTION_PADDING = SPACING_LARGE;
+
+export const SECTION_WIDTH = FULL_WIDTH - PAGE_PADDING * 2;
+
+export const COLOR_PRIMARY = '#f5333f';
+export const COLOR_SECONDARY = '#011e41';
+export const COLOR_TEXT = '#212121';
+export const COLOR_BACKGROUND = '#f0f0f0';
