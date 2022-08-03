9201dfa1aede8eed9536a59b0d4da918bc801d11
puranban <puranban77@gmail.com>
2022-08-03 14:40:05 +0545

    Change single quote in lang

--- src/root/lang.js
+++ src/root/lang.js
@@ -3684,7 +3684,7 @@ export default {
   finalReportChangeToOperationStrategy: 'Has there been any changes to the operation strategy',
   finalReportChangeToOperationStrategyExplain: 'Explain',
   finalReportTargetedPopulation: 'Target Population',
-  finalReportWomen: "Women",
+  finalReportWomen: 'Women',
   finalReportGirls: 'Girls (under 18)',
   finalReportMen: 'Men',
   finalReportBoys: 'Boys (under 18)',
