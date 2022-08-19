13c4bf678eaa48ab2eb9d448d789fa2bb910ec43
puranban <puranban77@gmail.com>
2022-08-19 18:04:42 +0545

    Hide export field in assesment

--- src/root/components/DrefPdfDocument/AboutServicesOutput/index.tsx
+++ src/root/components/DrefPdfDocument/AboutServicesOutput/index.tsx
@@ -11,12 +11,14 @@ import pdfStyles from '#utils/pdf/pdfStyles';
 interface Props {
   data: DrefApiFields;
   strings: Strings;
+  isAssessmentReport: boolean,
 }
 
 function AboutServicesOutput(props: Props) {
   const {
     data,
     strings,
+    isAssessmentReport,
   } = props;
 
   return (
@@ -52,35 +54,39 @@ function AboutServicesOutput(props: Props) {
                 </Text>
               </View>
             )}
-            {data?.logistic_capacity_of_ns && (
-              <View style={pdfStyles.qna}>
-                <Text style={pdfStyles.textLabelSection}>
-                  {strings.drefFormLogisticCapacityOfNs}
-                </Text>
-                <Text style={pdfStyles.answer}>
-                  {data.logistic_capacity_of_ns}
-                </Text>
-              </View>
-            )}
-            {data?.pmer && (
-              <View style={pdfStyles.qna}>
-                <Text style={pdfStyles.textLabelSection}>
-                  {strings.drefFormPmer}
-                </Text>
-                <Text style={pdfStyles.answer}>
-                  {data.pmer}
-                </Text>
-              </View>
-            )}
-            {data?.communication && (
-              <View style={pdfStyles.qna}>
-                <Text style={pdfStyles.textLabelSection}>
-                  {strings.drefFormCommunication}
-                </Text>
-                <Text style={pdfStyles.answer}>
-                  {data.communication}
-                </Text>
-              </View>
+            {isAssessmentReport && (
+              <>
+                {data?.logistic_capacity_of_ns && (
+                  <View style={pdfStyles.qna}>
+                    <Text style={pdfStyles.textLabelSection}>
+                      {strings.drefFormLogisticCapacityOfNs}
+                    </Text>
+                    <Text style={pdfStyles.answer}>
+                      {data.logistic_capacity_of_ns}
+                    </Text>
+                  </View>
+                )}
+                {data?.pmer && (
+                  <View style={pdfStyles.qna}>
+                    <Text style={pdfStyles.textLabelSection}>
+                      {strings.drefFormPmer}
+                    </Text>
+                    <Text style={pdfStyles.answer}>
+                      {data.pmer}
+                    </Text>
+                  </View>
+                )}
+                {data?.communication && (
+                  <View style={pdfStyles.qna}>
+                    <Text style={pdfStyles.textLabelSection}>
+                      {strings.drefFormCommunication}
+                    </Text>
+                    <Text style={pdfStyles.answer}>
+                      {data.communication}
+                    </Text>
+                  </View>
+                )}
+              </>
             )}
           </View>
         )}
