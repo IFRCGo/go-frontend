77bfdaa0facaed36817967bbb417c53e66627d71
puranban <puranban77@gmail.com>
2023-02-09 14:48:54 +0545

    Remove unused

--- src/root/views/Search/ProvinceList/index.tsx
+++ src/root/views/Search/ProvinceList/index.tsx
@@ -1,51 +1,50 @@
 import React from 'react';
-import {
-    IoChevronForwardOutline,
-} from 'react-icons/io5';
 
 import Container from '#components/Container';
-import Link from '#components/Link';
 import LanguageContext from '#root/languageContext';
 
 import styles from './styles.module.scss';
 
 export interface ProvinceResult {
-    id: number;
-    name: string;
-    score: number;
+  id: number;
+  name: string;
+  score: number;
 }
 
 interface Props {
-    data: ProvinceResult[] | undefined;
-    actions: React.ReactNode;
+  data?: ProvinceResult[];
+  actions: React.ReactNode;
 }
 
 function ProvinceList(props: Props) {
-    const {
-        data,
-        actions,
-    } = props;
+  const {
+    data,
+    actions,
+  } = props;
 
-    const { strings } = React.useContext(LanguageContext);
+  const { strings } = React.useContext(LanguageContext);
 
-    if (!data) {
-        return null;
-    }
+  if (!data) {
+    return null;
+  }
 
-    return (
-        <Container
-            heading={strings.searchIfrcProvince}
-            actions={actions}
-            sub
-            contentClassName={styles.provinceList}
+  return (
+    <Container
+      heading={strings.searchIfrcProvince}
+      actions={actions}
+      sub
+      contentClassName={styles.provinceList}
+    >
+      {data.map((district) => (
+        <div
+          key={district.id}
+          className={styles.provinceName}
         >
-            {data.map((district) => (
-                <div className={styles.provinceName}>
-                    {district.name}
-                </div>
-            ))}
-        </Container>
-    );
+          {district.name}
+        </div>
+      ))}
+    </Container>
+  );
 }
 
 export default ProvinceList;
