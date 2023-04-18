ebc877a4c0f864492c4bc93c592806cb91624855
priyesh777 <priyeshgautam1010@gmail.com>
2023-04-18 17:16:41 +0545

    Add new link components for UI

---
+++ src/root/views/GoUI/components/ButtonLikeLink/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function ButtonLikeLink() {
+    return (
+        <div>
+            This is the most challenging moment that shall pass.
+        </div>
+    );
+}
+export default ButtonLikeLink;
