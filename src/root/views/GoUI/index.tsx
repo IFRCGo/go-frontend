57e65303b563ef2d32bdce8848d481846472c9f2
priyesh777 <priyeshgautam1010@gmail.com>
2023-04-18 17:16:41 +0545

    Add new link components for UI

--- src/root/views/GoUI/index.tsx
+++ src/root/views/GoUI/index.tsx
@@ -1,18 +1,96 @@
 import React from 'react';
-import Link from './components/Link';
+import Link from './components/Links';
+import Header from '#components/Header';
 import styles from './styles.module.scss';
 
 function GoUI() {
     return (
-        <div>
-            <Link
-                className={styles.url}
-                href={'www.gooogle.com'}
-                target="_blank"
-            >
-                More Info
-            </Link>
-        </div>
+        <>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="REGULAR LINK"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="regular"
+                >
+                    View all emergencies (last 30 days)
+                </Link>
+            </div>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="HIGHLIGHTED LINK"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="highlighted"
+                >
+                    Special Link
+                </Link>
+            </div>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="POPUP"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="popup"
+                >
+                    POPUP LINK (COUNTRY)
+                </Link>
+            </div>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="TABLE"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="table"
+                >
+                    Phillipines - Emergency 1
+                </Link>
+            </div>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="TEXT LINK"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="textLink"
+                >
+                    Phillipines - Emergency 1
+                </Link>
+            </div>
+            <div className={styles.linksContainer}>
+                <Header
+                    heading="POPUP LINK"
+                    headingSize="small"
+                />
+                <Link
+                    className={styles.url}
+                    href={'www.gooogle.com'}
+                    target="_blank"
+                    variant="specialEmail"
+                >
+                    communications@ifrc.org
+                </Link>
+            </div>
+        </>
     );
 }
 
