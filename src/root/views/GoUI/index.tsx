00539c5befc874946230e62a8250e475aa48ccac
shreeyash07 <shresthashree0807@gmail.com>
2023-04-24 17:08:16 +0545

    Finalize card

--- src/root/views/GoUI/index.tsx
+++ src/root/views/GoUI/index.tsx
@@ -2,13 +2,19 @@ import Card from './components/Card';
 import React from 'react';
 import Links from './examples/Links';
 import styles from './styles.module.scss';
+import Cards from './examples/Cards';
 
 function GoUI() {
-    return (
-        <div className={styles.linksContainer}>
-          <Links />
-        </div>
-    );
+  return (
+    <>
+      <div className={styles.linksContainer}>
+        <Links />
+      </div>
+      <div>
+        <Cards />
+      </div>
+    </>
+  );
 }
 
 export default GoUI;
