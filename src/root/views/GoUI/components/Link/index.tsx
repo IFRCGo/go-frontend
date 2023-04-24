b87cb7a0250d80117eefc4d4d73a850ef972e413
priyesh777 <priyeshgautam1010@gmail.com>
2023-04-24 15:12:08 +0545

    Add button-like-link in the link collections

--- src/root/views/GoUI/components/Link/index.tsx
+++ src/root/views/GoUI/components/Link/index.tsx
@@ -5,23 +5,26 @@ import {
   IoChevronForward,
   IoOpenOutline,
 } from 'react-icons/io5';
-
+import ButtonLikeLink from '#components/ButtonLikeLink';
 
 import styles from './styles.module.scss';
 
-type LinkVariant = 'regular' | 'title' | 'text' | 'external';
+type LinkVariant = 'regular' | 'title' | 'text' | 'external' | 'secondary';
 
 const variantToIconMap: Record<LinkVariant, React.ReactElement | undefined> = {
   regular: <IoChevronForward />,
   title: <IoChevronForward />,
   external: <IoOpenOutline />,
+  secondary: undefined,
   text: undefined,
 };
+
 const variantStyleMap: Record<LinkVariant, string | undefined> = {
   regular: undefined,
   title: styles.title,
   text: styles.text,
   external: undefined,
+  secondary: undefined,
 };
 
 export interface Props extends RouterLinkProps {
@@ -43,10 +46,13 @@ function Link(props: Props) {
     actionsContainerClassName,
     iconsContainerClassName,
     linkElementClassName,
+    children,
     ...otherProps
   } = props;
 
   const icon = variantToIconMap[variant];
+
+  const specialLink = (variant === 'external') ||  (variant === 'secondary');
 
   return (
     <div className={_cs(className, styles.linkContainer, variantStyleMap[variant])}>
@@ -55,21 +61,39 @@ function Link(props: Props) {
           {icons}
         </div>
       )}
-      {variant === 'external' ? (
+      {variant === 'external' && (
         <a
           className={_cs(linkElementClassName, styles.link)}
           href={to as string}
           target="_blank"
           rel="noopener noreferrer external"
           {...otherProps}
-        />
-      ) : (
+        >
+          {children}
+        </a>
+      )}
+
+      {variant === 'secondary' && (
+        <ButtonLikeLink
+          className={_cs(linkElementClassName, styles.buttonLikeLink)}
+          to={to}
+          external
+          icons={icons}
+        >
+          {children}
+        </ButtonLikeLink>
+      )}
+
+      {!specialLink && (
         <InternalLink
           className={_cs(linkElementClassName, styles.link)}
           to={to}
           {...otherProps}
-        />
+        >
+          {children}
+        </InternalLink>
       )}
+
       {(actions || icon) && (
         <div
           className={_cs(actionsContainerClassName, styles.actions)}
