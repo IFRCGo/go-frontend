94deb9513060fdc7f44a854e378bafac80852006
priyesh777 <priyeshgautam1010@gmail.com>
2023-05-03 17:01:49 +0545

    Add new layout for ui elements

--- src/root/views/GoUI/index.tsx
+++ src/root/views/GoUI/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Links from './examples/Links';
 import Buttons from './examples/Buttons';
 import styles from './styles.module.scss';
@@ -6,27 +6,69 @@ import Cards from './examples/Cards';
 import Alerts from './examples/Alerts';
 import OverlayExample from './examples/OverlayExample';
 import Headings from './examples/Headings';
+import Button from './components/Button';
 
 function GoUI() {
+
+  const [element, setElement] = useState<string | undefined>("links");
+
+  const handleNewComponent = useCallback((name) => {
+    setElement(name);
+  }, [setElement]);
+
   return (
-    <div className={styles.elementWrapper}>
-      <div className={styles.elementsContainer}>
-        <Links />
+    <div className={styles.mainContainer}>
+      <div className={styles.sideContent}>
+        <Button
+          name="links"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Links
+        </Button>
+        <Button
+          name="buttons"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Buttons
+        </Button>
+        <Button
+          name="headings"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Headings
+        </Button>
+        <Button
+          name="cards"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Cards
+        </Button>
+        <Button
+          name="alerts"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Alerts
+        </Button>
+        <Button
+          name="overlay"
+          className={styles.option}
+          onClick={handleNewComponent}
+        >
+          Overlay Example
+        </Button>
       </div>
-      <div className={styles.elementsContainer}>
-        <Buttons />
-      </div>
-      <div className={styles.elementsContainer}>
-        <Cards />
-      </div>
-      <div className={styles.elementsContainer}>
-        <Alerts />
-      </div>
-      <div className={styles.elementsContainer}>
-        <OverlayExample />
-      </div>
-      <div>
-        <Headings />
+      <div className={styles.componentLayout}>
+        {element === "links" && <Links />}
+        {element === "cards" && <Cards />}
+        {element === "alerts" && <Alerts />}
+        {element === "overlay" && <OverlayExample />}
+        {element === "buttons" && <Buttons />}
+        {element === "headings" && <Headings />}
       </div>
     </div>
   );
