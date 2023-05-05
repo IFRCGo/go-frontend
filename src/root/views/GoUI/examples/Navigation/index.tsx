8f22fba0d665375408709dd141667348f96ff488
shreeyash07 <shresthashree0807@gmail.com>
2023-05-05 16:29:06 +0545

    Add step variant

--- src/root/views/GoUI/examples/Navigation/index.tsx
+++ src/root/views/GoUI/examples/Navigation/index.tsx
@@ -9,12 +9,15 @@ import TabPanel from '#views/GoUI/components/Tabs/TabPanel';
 
 function Navigation() {
   const [activeTab, setActiveTab] = React.useState<'tab1' | 'tab2'>('tab1');
-  console.log(activeTab);
   return (
     <div className={styles.navigationCollection}>
       <Header
         heading="NAVIGATION COLLECTION"
         headingSize="medium"
+      />
+      <Header
+        heading="PRIMARY"
+        headingSize="small"
       />
       <div className={styles.navigationContainer}>
         <Tabs
@@ -24,10 +27,62 @@ function Navigation() {
         >
           <TabList>
             <Tab name="tab1">
-              Tab1
+              Tab 1
             </Tab>
             <Tab name="tab2">
-              Tab2
+              Tab 2
+            </Tab>
+          </TabList>
+          <TabPanel name="tab1">
+            This is tab for Tab 1
+          </TabPanel>
+          <TabPanel name="tab2">
+            This is tab for Tab 2
+          </TabPanel>
+        </Tabs>
+      </div>
+      <Header
+        heading="SECONDARY"
+        headingSize="small"
+      />
+      <div className={styles.navigationContainer}>
+        <Tabs
+          value={activeTab}
+          onChange={setActiveTab}
+          variant="secondary"
+        >
+          <TabList>
+            <Tab name="tab1">
+              Tab 1
+            </Tab>
+            <Tab name="tab2">
+              Tab 2
+            </Tab>
+          </TabList>
+          <TabPanel name="tab1">
+            This is tab for Tab 1
+          </TabPanel>
+          <TabPanel name="tab2">
+            This is tab for Tab 2
+          </TabPanel>
+        </Tabs>
+      </div>
+      <Header
+        heading="STEP"
+        headingSize="small"
+      />
+      <div className={styles.navigationContainer}>
+        <Tabs
+          value={activeTab}
+          onChange={setActiveTab}
+          variant="step"
+        >
+          <TabList>
+            <Tab name="tab1">
+              Tab 1
+            </Tab>
+            <Tab name="tab2">
+              Tab 2
             </Tab>
           </TabList>
           <TabPanel name="tab1">
