88c38b928a939c7ab5a908e7a954b311adf65697
priyesh777 <priyeshgautam1010@gmail.com>
2023-05-10 12:00:04 +0545

    Add textInput component for form field

---
+++ src/root/views/GoUI/components/TextInput/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import InputContainer, { Props as InputContainerProps } from '#goui/components/InputContainer';
+import RawInput, { Props as RawInputProps } from '#goui/components/RawInput';
+
+type InheritedProps<T> = (Omit<InputContainerProps, 'input'> & RawInputProps<T>);
+export interface Props<T extends string | undefined> extends InheritedProps<T> {
+  inputElementRef?: React.RefObject<HTMLInputElement>;
+  inputClassName?: string;
+}
+
+function TextInput<T extends string | undefined>(props: Props<T>) {
+  const {
+    className,
+    actions,
+    icons,
+    error,
+    hint,
+    label,
+    disabled,
+    readOnly,
+    errorOnTooltip,
+    inputClassName,
+    type = 'text',
+    variant,
+    ...otherInputProps
+  } = props;
+
+  return (
+    <InputContainer
+      className={className}
+      actions={actions}
+      icons={icons}
+      hint={hint}
+      error={error}
+      label={label}
+      disabled={disabled}
+      errorOnTooltip={errorOnTooltip}
+      variant={variant}
+      input={(
+        <RawInput
+          {...otherInputProps}
+          readOnly={readOnly}
+          disabled={disabled}
+          className={inputClassName}
+          type={type}
+        />
+      )}
+    />
+  );
+}
+
+export default TextInput;
